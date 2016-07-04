f714b72cc6bde9103c1342df2169accfacc4c44e
SolomonShorser-OICR <solomon.shorser@oicr.on.ca>
2016-07-04 14:41:25 -0400

    Update unit tests and dependencies.

--- src/test/java/org/reactome/addlinks/test/TestFileRetriever.java
+++ src/test/java/org/reactome/addlinks/test/TestFileRetriever.java
@@ -5,6 +5,8 @@ package org.reactome.addlinks.test;
 
 import static org.junit.Assert.*;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 import java.net.URI;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -12,15 +14,32 @@ import java.time.Duration;
 import java.time.temporal.ChronoUnit;
 import java.util.Random;
 
+import org.apache.commons.net.ftp.FTPClient;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PowerMockIgnore;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 import org.reactome.addlinks.dataretrieval.DataRetriever;
 import org.reactome.addlinks.dataretrieval.FileRetriever;
+import static org.mockito.Matchers.*;
 
 /**
  * @author sshorser
  *
  */
+@PowerMockIgnore({"javax.management.*","javax.net.ssl.*"})
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({ java.net.URI.class, org.apache.commons.net.ftp.FTPClient.class, org.reactome.addlinks.dataretrieval.FileRetriever.class})
 public class TestFileRetriever {
+
+	@Mock
+	FTPClient mockFtpClient ;
+	
 
 	/**
 	 * Test method for {@link org.reactome.addlinks.dataretrieval.FileRetriever#fetchData()}.
@@ -52,5 +71,46 @@ public class TestFileRetriever {
 		//check that the file exists.
 		assertTrue(Files.exists(Paths.get(dest)));
 	}
+
+	
+	@Before
+	public void setup() {
+		MockitoAnnotations.initMocks(this);
+	}
+
+	
+	@Test
+	public void testFetchFTPData() throws Exception
+	{
+		//set up the mock client
+		//FTPClient mockFtpClient = PowerMockito.mock(FTPClient.class);
+		PowerMockito.doNothing().when(mockFtpClient).connect(anyString());
+		PowerMockito.when(mockFtpClient.login(anyString(),anyString())).thenReturn(true);
+		PowerMockito.when(mockFtpClient.getReplyCode()).thenReturn(220);
+		PowerMockito.when(mockFtpClient.getReplyString()).thenReturn("220 reply string");
+		InputStream inStream = new ByteArrayInputStream("this is a test".getBytes());
+		PowerMockito.when(mockFtpClient.retrieveFileStream(anyString())).thenReturn(inStream);
+		//when creating a new FTPClient, always return the mockClient.
+		PowerMockito.whenNew(FTPClient.class).withAnyArguments().thenReturn(mockFtpClient);
+		PowerMockito.whenNew(FTPClient.class).withNoArguments().thenReturn(mockFtpClient);
+
+		DataRetriever retriever = new FileRetriever();
+		PowerMockito.mockStatic(URI.class);
+		URI mockUri = PowerMockito.mock(URI.class);
+		
+		PowerMockito.doReturn("ftp").when(mockUri).getScheme();
+		PowerMockito.when(mockUri.getHost()).thenReturn("testhost");
+		PowerMockito.doReturn("/some/path").when(mockUri).getPath();
+		
+		retriever.setDataURL(mockUri);
+		String dest = "/tmp/testFetchData_"+ String.valueOf((new Random()).nextInt(Integer.MAX_VALUE));
+		retriever.setFetchDestination(dest);
+		Duration age = Duration.of(1,ChronoUnit.SECONDS);
+		retriever.setMaxAge(age);
+		
+		retriever.fetchData();
+		assertTrue(Files.exists(Paths.get(dest)));
+	}
+	
 
 }
