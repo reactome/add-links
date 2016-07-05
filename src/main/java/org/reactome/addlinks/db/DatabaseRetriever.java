ddf16aabd9031c1038a1307700093b4075780147
SolomonShorser-OICR <solomon.shorser@oicr.on.ca>
2016-07-05 17:05:46 -0400

    Implemented database cache
     - Restored the dependency to the ReactomeCore library.
     - Added a new cache to retrieve and store ReferenceGeneProduct shells.
     - Added a simple test class for the database cache.

--- src/main/java/org/reactome/addlinks/db/DatabaseRetriever.java
+++ src/main/java/org/reactome/addlinks/db/DatabaseRetriever.java
@@ -3,11 +3,11 @@ package org.reactome.addlinks.db;
 import java.util.List;
 import java.util.stream.Collectors;
 
-//import org.gk.model.GKInstance;
-//import org.gk.model.Instance;
-//import org.gk.persistence.MySQLAdaptor;
-//import org.reactome.core.factory.DatabaseObjectFactory;
-//import org.reactome.core.model.DatabaseObject;
+import org.gk.model.GKInstance;
+import org.gk.model.Instance;
+import org.gk.persistence.MySQLAdaptor;
+import org.reactome.core.factory.DatabaseObjectFactory;
+import org.reactome.core.model.DatabaseObject;
 
 public class DatabaseRetriever {
 
@@ -16,9 +16,20 @@ public class DatabaseRetriever {
 	private String username;
 	private String password;
 
+	/*Big important query:
+	 * 
+select _displayName, _class, DatabaseObject.db_id as object_db_id, ReferenceEntity.identifier, ReferenceEntity.referenceDatabase as refent_refdb, ReferenceSequence.species -- , DatabaseIdentifier.identifier as ref_identifier -- , DatabaseIdentifier.referenceDatabase db_id_refdb
+from DatabaseObject
+inner join ReferenceGeneProduct on ReferenceGeneProduct.db_id = DatabaseObject.db_id 
+inner join ReferenceEntity on ReferenceEntity.db_id = ReferenceGeneProduct.db_id
+inner join ReferenceSequence on ReferenceSequence.db_id = ReferenceEntity.db_id
+-- left join DatabaseIdentifier on DatabaseIdentifier.identifier = ReferenceEntity.identifier
+where _class  = 'ReferenceGeneProduct'; 
+	 */
+	
 	public List<Long> getIDs(String className) throws Exception
 	{
-//		MySQLAdaptor adapter = new MySQLAdaptor(host, database, username, password);
+		MySQLAdaptor adapter = new MySQLAdaptor(host, database, username, password);
 //		
 //		@SuppressWarnings("unchecked")
 //		List<Instance> instances =  (List<Instance>) adapter.fetchInstancesByClass(className);
