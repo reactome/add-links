cd49524daa8f88a2f4f9167e8b90274e94805cbe
SolomonShorser-OICR <solomon.shorser@oicr.on.ca>
2016-07-07 17:13:47 -0400

    Process files that were downloaded.

--- src/main/java/org/reactome/addlinks/AddLinks.java
+++ src/main/java/org/reactome/addlinks/AddLinks.java
@@ -1,10 +1,13 @@
 package org.reactome.addlinks;
 
+import java.nio.file.Paths;
 import java.util.Map;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.reactome.addlinks.dataretrieval.FileRetriever;
+import org.reactome.addlinks.fileprocessors.FlyBaseFileProcessor;
+import org.reactome.addlinks.fileprocessors.PROFileProcessor;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -25,10 +28,22 @@ public class AddLinks {
 			}
 			catch (Exception e)
 			{
+				//TODO: The decision to continue after a failure should be a configurable option. 
 				logger.info("Exception caught while processing {}, message is: {}. Will continue with next file retriever.",key,e.getMessage());
 			}
 		}
 		logger.info("Finished downloading files.");
+		
+		logger.info("Now processing the files...");
+
+		PROFileProcessor proFileProcessor = new PROFileProcessor();
+		proFileProcessor.setPath(Paths.get("/tmp/uniprotmapping.txt"));
+		Map<String,String> proMappings = proFileProcessor.getIdMappingsFromFile();
+
+		FlyBaseFileProcessor flyBaseFileProcessor = new FlyBaseFileProcessor();
+		flyBaseFileProcessor.setPath(Paths.get("/tmp/FlyBase.tsv.gz"));
+		Map<String,String> flyBaseMappings = flyBaseFileProcessor.getIdMappingsFromFile();		
+		
 		context.close();
 	}
 
