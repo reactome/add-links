e6f4acaaf504ac68bd658e8a6104d6fd1b908d0e
SolomonShorser-OICR <solomon.shorser@oicr.on.ca>
2016-07-19 14:14:29 -0400

    Improved unzipping
    The FileProcessor can now handle unzipping multiple files in a single
    ZIP archive. This is needed for HMDB which will provide us with a ZIP
    archive containing thousands of XML files.

--- src/main/java/org/reactome/addlinks/fileprocessors/FileProcessor.java
+++ src/main/java/org/reactome/addlinks/fileprocessors/FileProcessor.java
@@ -5,14 +5,25 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.attribute.FileAttribute;
+import java.nio.file.attribute.FileAttributeView;
+import java.nio.file.attribute.FileStoreAttributeView;
+import java.nio.file.attribute.PosixFilePermission;
+import java.nio.file.attribute.PosixFilePermissions;
 import java.util.Map;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
 import java.util.zip.GZIPInputStream;
 import java.util.zip.InflaterInputStream;
+import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.hamcrest.core.IsSame;
 
 public abstract class FileProcessor
 {
@@ -27,36 +38,75 @@ public abstract class FileProcessor
 	
 	public abstract Map<String,String> getIdMappingsFromFile();
 	
-	public String unzipFile(Path p)
+	public String unzipFile(Path p) throws Exception 
 	{
 		String fileName = null ;
 		logger.debug("Unzipping {}",p);
 
-		String extension = p.toString().substring(p.toString().lastIndexOf("."));
+		String extension = p.toString().substring(p.toString().lastIndexOf(".")).toLowerCase();
 		String outFileName = p.toString().replace(extension, "");
 		
 		logger.debug("unzipping as {}",extension);
+		if (!extension.equals(".gz") && !(extension.equals(".zip")))
+		{
+			logger.error("The file extension {} for the file {} is not supported by this function.",extension, p.getFileName());
+			throw new Exception("Unsupported file extension was received by unzip function: " + extension);
+		}
+
 		// If the extension is .gz or .gzip, use GZIPInputStream to read the file,
 		// otherwise, use ZipInputStream.
-		try (InflaterInputStream gzIn = (extension.equals(".gz") || extension.equals(".gzip"))
+		try (InflaterInputStream inflaterStream = (extension.equals(".gz") || extension.equals(".gzip"))
 										? new GZIPInputStream(new FileInputStream(p.toFile()))
 										: new ZipInputStream(new FileInputStream(p.toFile()));
-			FileOutputStream outStream = new FileOutputStream( new File(outFileName) );)
+			)
 		{
-
-			byte[] buffer = new byte[1024];	
-			while ( gzIn.read(buffer) > 0)
+			//This Consumer will write a zip stream to a file. 
+			BiConsumer<InflaterInputStream,String> dataWriter = (inStream,outputFileName) ->
 			{
-				outStream.write(buffer);
+				byte[] buffer = new byte[1024];
+				try
+				{
+					int count = 0;
+					try(FileOutputStream outStream = new FileOutputStream( new File(outputFileName)); )
+					{
+						while ((count = inStream.read(buffer)) > 0)
+						{
+								outStream.write(buffer,0,count);
+						}
+					}
+				}
+				catch (Exception e)
+				{
+					logger.error("Error writing zip file: {}",e.getMessage());
+				}
+			};
+
+			//If it's a ZIP file, there could be multiple files but if it's a GZIP file, there is only one file.
+			if (inflaterStream instanceof ZipInputStream)
+			{
+				if (!Files.exists(Paths.get(outFileName)))
+				{
+					Files.createDirectory(Paths.get(outFileName));
+				}
+				ZipEntry entry;
+				int i=0;
+				while( (entry = ((ZipInputStream)inflaterStream).getNextEntry()) != null )
+				{
+					i++;
+					dataWriter.accept(inflaterStream, outFileName+"/"+entry.getName());
+				}
+				logger.info("Extracted {} files from zip archive.",i);
 			}
+			else
+			{
+				dataWriter.accept(inflaterStream, outFileName);
+			}
+
 			fileName = outFileName;
-		}
-		catch (FileNotFoundException e)
-		{
-			e.printStackTrace();
 		}
 		catch (IOException e)
 		{
+			logger.error("Exception was caught while trying to unzip a file: {}",e.getMessage());
 			e.printStackTrace();
 		}
 
