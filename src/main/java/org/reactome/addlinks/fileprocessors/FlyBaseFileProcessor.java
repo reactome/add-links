15cbf06b4c2d71990becb6937794685c8a8703dd
SolomonShorser-OICR <solomon.shorser@oicr.on.ca>
2016-07-19 15:37:22 -0400

    cleanup

--- src/main/java/org/reactome/addlinks/fileprocessors/FlyBaseFileProcessor.java
+++ src/main/java/org/reactome/addlinks/fileprocessors/FlyBaseFileProcessor.java
@@ -21,46 +21,32 @@ public class FlyBaseFileProcessor extends FileProcessor
 		try
 		{
 			String unzippedFile = this.unzipFile(this.pathToFile);
-	//		if (unzippedFiles.size() > 1)
-	//		{
-	//			logger.error("FlyBase file processor was only expecting one file, but the zipped file somehow contained more: {}", String.join(", ", unzippedFiles));
-	//			throw new RuntimeException("Too many input files!");
-	//		}
-	//		else if (unzippedFiles.size() == 0)
-	//		{
-	//			logger.error("File processor received no names of unzipped files!");
-	//			throw new RuntimeException("Not enough input files!");
-	//		}
-			
-	//		unzippedFiles.stream().forEach( s -> 
-	//		{
-				AtomicInteger lineCount = new AtomicInteger(0);
-				try
+			AtomicInteger lineCount = new AtomicInteger(0);
+			try
+			{
+				//I added ".sequential()" because I have line counter and I want to ensure that it works properly.
+				Files.lines(Paths.get(unzippedFile)).filter(p -> !p.startsWith("#")).sequential().forEach( line ->
 				{
-					//I added ".sequential()" because I have line counter and I want to ensure that it works properly.
-					Files.lines(Paths.get(unzippedFile)).filter(p -> !p.startsWith("#")).sequential().forEach( line ->
+					lineCount.set(lineCount.get() + 1);
+					String[] parts = line.split("\\t");
+					// UniProt ID is the last column - if split returns 5 things than there is a UniProt ID. 
+					if (parts.length==5)
 					{
-						lineCount.set(lineCount.get() + 1);
-						String[] parts = line.split("\\t");
-						// UniProt ID is the last column - if split returns 5 things than there is a UniProt ID. 
-						if (parts.length==5)
+						if (!"".equals(parts[4].trim()))
 						{
-							if (!"".equals(parts[4].trim()))
-							{
-								mappings.put(parts[4], parts[1]);
-							}
-							else
-							{
-								logger.warn("Could not extract the UniProt ID from line #{}", lineCount.get());
-							}
+							mappings.put(parts[4], parts[1]);
 						}
-					});
-				}
-				catch (IOException e)
-				{
-					e.printStackTrace();
-				}	
-	//		});
+						else
+						{
+							logger.warn("Could not extract the UniProt ID from line #{}", lineCount.get());
+						}
+					}
+				});
+			}
+			catch (IOException e)
+			{
+				e.printStackTrace();
+			}	
 			logger.debug("Number of UniProt IDs in mapping: {}; Number of lines processed: {}",mappings.keySet().size(), lineCount.get());
 		}
 		catch (Exception e)
