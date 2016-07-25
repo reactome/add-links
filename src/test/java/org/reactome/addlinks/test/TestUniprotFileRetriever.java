f8fdd8fc51491c1b3c62369eecd2f02be5cf49b2
SolomonShorser-OICR <solomon.shorser@oicr.on.ca>
2016-07-25 15:02:42 -0400

    Parameterize mapping ; Handle Locations with no query strings.

--- src/test/java/org/reactome/addlinks/test/TestUniprotFileRetriever.java
+++ src/test/java/org/reactome/addlinks/test/TestUniprotFileRetriever.java
@@ -1,8 +1,12 @@
 package org.reactome.addlinks.test;
 
+import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.time.Duration;
+import java.util.List;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -22,8 +26,9 @@ public class TestUniprotFileRetriever
 {
 
 	@Test
-	public void testUniProtFileRetriever() throws URISyntaxException
+	public void testUniProtFileRetriever() throws URISyntaxException, IOException
 	{	
+		//Some logging/debugging for monitoring the connection to the server.
 		System.setProperty("org.apache.commons.logging.Log","org.apache.commons.logging.impl.SimpleLog");
 		System.setProperty("org.apache.commons.logging.simplelog.showdatetime", "true");
 		System.setProperty("org.apache.commons.logging.simplelog.log.org.apache.http.wire", "DEBUG");
@@ -35,9 +40,20 @@ public class TestUniprotFileRetriever
 		
 		UniprotFileRetreiver retriever = new UniprotFileRetreiver();
 		retriever.setDataURL(new URI("http://www.uniprot.org/uploadlists/"));
-		retriever.setFetchDestination("/tmp/uniprot_xrefs.txt");
+		String mapFrom="ACC+ID";
+		String mapTo="ENSEMBL_ID";
+		String pathToFile = "/tmp/uniprot_mapping_service/uniprot_xrefs_"+mapFrom+"_to_"+mapTo+".txt";
+		retriever.setFetchDestination(pathToFile);
 		retriever.setMaxAge(Duration.ofSeconds(1));
-		
+		retriever.setMapFromDb(mapFrom);
+		retriever.setMapToDb(mapTo);
 		retriever.downloadData();
+		
+		System.out.println("Contents of "+pathToFile);
+		List<String> lines = Files.readAllLines(Paths.get(pathToFile));
+		for (String line : lines)
+		{
+			System.out.println(line);
+		}
 	}
 }
