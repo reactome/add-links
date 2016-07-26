7a5f69e3f3d0381ed49eed74b1c29ef8d906d893
SolomonShorser-OICR <solomon.shorser@oicr.on.ca>
2016-07-26 13:52:13 -0400

    Add new caches: refDB mapping and species mapping.

--- src/main/java/org/reactome/addlinks/db/ReferenceGeneProductCache.java
+++ src/main/java/org/reactome/addlinks/db/ReferenceGeneProductCache.java
@@ -5,108 +5,15 @@ import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.gk.persistence.MySQLAdaptor;
 
-public final class ReferenceGeneProductCache {
-	private static final Logger logger = LogManager.getLogger();
-	private static ReferenceGeneProductCache cache;
-	
-	private static final String query = "select _displayName, _class, DatabaseObject.db_id as object_db_id, ReferenceEntity.identifier, ReferenceEntity.referenceDatabase as refent_refdb, ReferenceSequence.species " +
-										" from DatabaseObject "+
-										" inner join ReferenceGeneProduct on ReferenceGeneProduct.db_id = DatabaseObject.db_id "+ 
-										" inner join ReferenceEntity on ReferenceEntity.db_id = ReferenceGeneProduct.db_id "+
-										" inner join ReferenceSequence on ReferenceSequence.db_id = ReferenceEntity.db_id "+
-										" where _class  = \'ReferenceGeneProduct\'; ";
-	
-	private static String host;
-	private static String database;
-	private static String username;
-	private static String password;
-	private static int port;
-	
-	public static synchronized ReferenceGeneProductCache getInstance() 
-	{
-		if (ReferenceGeneProductCache.cache == null)
-		{
-			logger.info("Cache is not initialized. Will initialize now.");
-			ReferenceGeneProductCache.cache = new ReferenceGeneProductCache();
-		}
-		else
-		{
-			logger.info("Cache is already initialized.");
-		}
-		
-		return ReferenceGeneProductCache.cache;
-	}
-	
-	public static void setDbParams(String host, String database, String username, String password, int port)
-	{
-		ReferenceGeneProductCache.host = host;
-		ReferenceGeneProductCache.database = database;
-		ReferenceGeneProductCache.username = username;
-		ReferenceGeneProductCache.password = password;
-		ReferenceGeneProductCache.port = port;
-	}
-	
-	private ReferenceGeneProductCache()
-	{
-		try {
-			MySQLAdaptor adapter = new MySQLAdaptor(ReferenceGeneProductCache.host, ReferenceGeneProductCache.database, ReferenceGeneProductCache.username, ReferenceGeneProductCache.password, ReferenceGeneProductCache.port);
-			ResultSet rs = adapter.executeQuery(ReferenceGeneProductCache.query, null);
-			
-			while (rs.next())
-			{
-				ReferenceGeneProductShell shell = new ReferenceGeneProductShell();
-				shell.setDbId(rs.getString("object_db_id"));
-				shell.setDisplayName(rs.getString("_displayName"));
-				shell.setIdentifier(rs.getString("identifier"));
-				shell.setReactomeClass(rs.getString("_class"));
-				shell.setReferenceDatabase(rs.getString("refent_refdb"));
-				shell.setSpecies(rs.getString("species"));
-				
-				//Now, insert into the caches.
-				//
-				//Species Cache
-				//If this species is not yet cached...
-				if (! ReferenceGeneProductCache.cacheBySpecies.containsKey(shell.getSpecies()))
-				{
-					List<ReferenceGeneProductShell> bySpecies = new LinkedList<ReferenceGeneProductShell>();
-					bySpecies.add(shell);
-					ReferenceGeneProductCache.cacheBySpecies.put(shell.getSpecies(), bySpecies);
-				}
-				else
-				{
-					ReferenceGeneProductCache.cacheBySpecies.get(shell.getSpecies()).add(shell);
-				}
-				//ReferenceDatabase Cache
-				//If this species is not yet cached...
-				if (! ReferenceGeneProductCache.cacheByRefDb.containsKey(shell.getReferenceDatabase()))
-				{
-					List<ReferenceGeneProductShell> byRefDb = new LinkedList<ReferenceGeneProductShell>();
-					byRefDb.add(shell);
-					ReferenceGeneProductCache.cacheByRefDb.put(shell.getReferenceDatabase(), byRefDb);
-				}
-				else
-				{
-					ReferenceGeneProductCache.cacheByRefDb.get(shell.getReferenceDatabase()).add(shell);
-				}
-				// ID Cache
-				ReferenceGeneProductCache.cacheById.put(shell.getDbId(), shell);
-			}
-			adapter.cleanUp();
-			logger.info("Caches initialized. cache-by-refdb size: {}; cache-by-species size: {}; cache-by-id size: {}",
-							ReferenceGeneProductCache.cacheByRefDb.size(),
-							ReferenceGeneProductCache.cacheBySpecies.size(),
-							ReferenceGeneProductCache.cacheById.size());
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
+public final class ReferenceGeneProductCache 
+{
 	
 	public class ReferenceGeneProductShell
 	{
@@ -155,26 +62,177 @@ public final class ReferenceGeneProductCache {
 		}
 	}
 	
+	private static final Logger logger = LogManager.getLogger();
+	private static ReferenceGeneProductCache cache;
+	
+	private static final String query = "select _displayName, _class, DatabaseObject.db_id as object_db_id, ReferenceEntity.identifier, ReferenceEntity.referenceDatabase as refent_refdb, ReferenceSequence.species " +
+										" from DatabaseObject "+
+										" inner join ReferenceGeneProduct on ReferenceGeneProduct.db_id = DatabaseObject.db_id "+ 
+										" inner join ReferenceEntity on ReferenceEntity.db_id = ReferenceGeneProduct.db_id "+
+										" inner join ReferenceSequence on ReferenceSequence.db_id = ReferenceEntity.db_id "+
+										" where _class  = \'ReferenceGeneProduct\'; ";
+	
+	private static final String refdbMappingQuery = " select distinct _displayName, db_id from DatabaseObject where db_id in (select ReferenceDatabase_2_name.DB_ID from ReferenceDatabase_2_name) order by _displayName asc; ";
+	
+	private static final String speciesMappingQuery = " select distinct _displayName, db_id from DatabaseObject where db_id in (select Taxon_2_name.db_id from Taxon_2_name) order by _displayName asc; ";
+	
+	private static String host;
+	private static String database;
+	private static String username;
+	private static String password;
+	private static int port;
+	
+	public static synchronized ReferenceGeneProductCache getInstance() 
+	{
+		if (ReferenceGeneProductCache.cache == null)
+		{
+			logger.info("Cache is not initialized. Will initialize now.");
+			ReferenceGeneProductCache.cache = new ReferenceGeneProductCache();
+		}
+		else
+		{
+			logger.info("Cache is already initialized.");
+		}
+		
+		return ReferenceGeneProductCache.cache;
+	}
+	
+	public static void setDbParams(String host, String database, String username, String password, int port)
+	{
+		ReferenceGeneProductCache.host = host;
+		ReferenceGeneProductCache.database = database;
+		ReferenceGeneProductCache.username = username;
+		ReferenceGeneProductCache.password = password;
+		ReferenceGeneProductCache.port = port;
+	}
+	
+	private ReferenceGeneProductCache()
+	{
+		try
+		{
+			MySQLAdaptor adapter = new MySQLAdaptor(ReferenceGeneProductCache.host, ReferenceGeneProductCache.database, ReferenceGeneProductCache.username, ReferenceGeneProductCache.password, ReferenceGeneProductCache.port);
+			ResultSet rs = adapter.executeQuery(ReferenceGeneProductCache.query, null);
+			
+			while (rs.next())
+			{
+				ReferenceGeneProductShell shell = new ReferenceGeneProductShell();
+				shell.setDbId(rs.getString("object_db_id"));
+				shell.setDisplayName(rs.getString("_displayName"));
+				shell.setIdentifier(rs.getString("identifier"));
+				shell.setReactomeClass(rs.getString("_class"));
+				shell.setReferenceDatabase(rs.getString("refent_refdb"));
+				shell.setSpecies(rs.getString("species"));
+				
+				//Now, insert into the caches.
+				//
+				//Species Cache
+				//If this species is not yet cached...
+				if (! ReferenceGeneProductCache.cacheBySpecies.containsKey(shell.getSpecies()))
+				{
+					List<ReferenceGeneProductShell> bySpecies = new LinkedList<ReferenceGeneProductShell>();
+					bySpecies.add(shell);
+					ReferenceGeneProductCache.cacheBySpecies.put(shell.getSpecies(), bySpecies);
+				}
+				else
+				{
+					ReferenceGeneProductCache.cacheBySpecies.get(shell.getSpecies()).add(shell);
+				}
+				//ReferenceDatabase Cache
+				//If this species is not yet cached...
+				if (! ReferenceGeneProductCache.cacheByRefDb.containsKey(shell.getReferenceDatabase()))
+				{
+					List<ReferenceGeneProductShell> byRefDb = new LinkedList<ReferenceGeneProductShell>();
+					byRefDb.add(shell);
+					ReferenceGeneProductCache.cacheByRefDb.put(shell.getReferenceDatabase(), byRefDb);
+				}
+				else
+				{
+					ReferenceGeneProductCache.cacheByRefDb.get(shell.getReferenceDatabase()).add(shell);
+				}
+				// ID Cache
+				ReferenceGeneProductCache.cacheById.put(shell.getDbId(), shell);
+			}
+			rs.close();
+			
+			ResultSet refDbResultSet = adapter.executeQuery(ReferenceGeneProductCache.refdbMappingQuery,null);
+			while (refDbResultSet.next())
+			{
+				ReferenceGeneProductCache.refdbMapping.put(refDbResultSet.getString("db_id"),refDbResultSet.getString("_displayName"));
+			}
+			refDbResultSet.close();
+			
+			
+			ResultSet speciesResultSet = adapter.executeQuery(ReferenceGeneProductCache.speciesMappingQuery,null);
+			while (speciesResultSet.next())
+			{
+				ReferenceGeneProductCache.speciesMapping.put(speciesResultSet.getString("db_id"),speciesResultSet.getString("_displayName"));
+			}
+			speciesResultSet.close();
+			
+			adapter.cleanUp();
+			logger.info("Caches initialized."
+					+ " Keys in cache-by-refdb: {};"
+					+ " keys in cache-by-species: {};"
+					+ " keys in cache-by-id: {};"
+					+ " keys in refDbMapping: {};"
+					+ " keys in speciesMapping: {}",
+							ReferenceGeneProductCache.cacheByRefDb.size(),
+							ReferenceGeneProductCache.cacheBySpecies.size(),
+							ReferenceGeneProductCache.cacheById.size(),
+							ReferenceGeneProductCache.refdbMapping.size(),
+							ReferenceGeneProductCache.speciesMapping.size());
+		}
+		catch (Exception e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 	
 	private static Map<String,List<ReferenceGeneProductShell>> cacheBySpecies = new HashMap<String,List<ReferenceGeneProductShell>>();
 	private static Map<String,List<ReferenceGeneProductShell>> cacheByRefDb = new HashMap<String,List<ReferenceGeneProductShell>>();
 	private static Map<String,ReferenceGeneProductShell> cacheById = new HashMap<String,ReferenceGeneProductShell>();
 	
+	//also need some secondary mappings: species name-to-id and refdb name-to-id
+	private static Map<String,String> speciesMapping = new HashMap<String,String>();
+	private static Map<String,String> refdbMapping = new HashMap<String,String>();
+	
+	/**
+	 * Get a list of ReferenceGeneProduct shells keyed by Reference Database.
+	 * @param refDb
+	 * @return
+	 */
 	public List<ReferenceGeneProductShell> getByRefDb(String refDb)
 	{
 		return ReferenceGeneProductCache.cacheByRefDb.get(refDb);
 	}
 	
+	/**
+	 * Get a list of ReferenceGeneProduct shells keyed by Species.
+	 * @param species
+	 * @return
+	 */
 	public List<ReferenceGeneProductShell> getBySpecies(String species)
 	{
 		return ReferenceGeneProductCache.cacheBySpecies.get(species);
 	}
 	
+	/**
+	 * Get a ReferenceGeneProduct shell by its DB_ID.
+	 * @param id
+	 * @return
+	 */
 	public ReferenceGeneProductShell getById(String id)
 	{
 		return ReferenceGeneProductCache.cacheById.get(id);
 	}
 	
+	/**
+	 * Retreive a list of ReferenceGeneProduct shells filtered by Reference Database <em>and</em> Species.
+	 * @param refDb
+	 * @param species
+	 * @return
+	 */
 	public List<ReferenceGeneProductShell> getByRefDbAndSpecies(String refDb, String species)
 	{
 		//Get a list by referenceDatabase
@@ -183,5 +241,41 @@ public final class ReferenceGeneProductCache {
 		List<ReferenceGeneProductShell> objectsByRefDbAndSpecies = byRefDb.stream().filter(p -> p.getSpecies().equals(species)).collect(Collectors.toList());
 		
 		return objectsByRefDbAndSpecies;
+	}
+	
+	/**
+	 * Returns a set of the keys used to cache by Reference Database.
+	 * @return
+	 */
+	public Set<String> getListOfRefDbs()
+	{
+		return ReferenceGeneProductCache.cacheByRefDb.keySet();
+	}
+
+	/**
+	 * Returns a map of Reference Database names, keyed by their DB_IDs.
+	 * @return
+	 */
+	public Map<String,String> getRefDBMappings()
+	{
+		return ReferenceGeneProductCache.refdbMapping;
+	}
+	
+	/**
+	 * Returns a set of the keys used to cache by Species.
+	 * @return
+	 */
+	public Set<String> getListOfSpecies()
+	{
+		return ReferenceGeneProductCache.cacheBySpecies.keySet();
+	}
+	
+	/**
+	 * Returns a map of Species names, keyed by their DB_IDs.
+	 * @return
+	 */
+	public Map<String,String> getSpeciesMappings()
+	{
+		return ReferenceGeneProductCache.speciesMapping;
 	}
 }
