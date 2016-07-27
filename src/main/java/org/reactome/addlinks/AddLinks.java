8630965ffdca8333f339c7152ebc0be9f4592754
SolomonShorser-OICR <solomon.shorser@oicr.on.ca>
2016-07-27 14:05:38 -0400

    Experiment with running file retrievers in parallel.

--- src/main/java/org/reactome/addlinks/AddLinks.java
+++ src/main/java/org/reactome/addlinks/AddLinks.java
@@ -4,6 +4,7 @@ import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.nio.file.Paths;
 import java.time.Duration;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -30,12 +31,13 @@ public class AddLinks {
 	public static void main(String[] args) throws Exception {
 		
 		ConfigurableApplicationContext context = new ClassPathXmlApplicationContext("application-context.xml");
+		//TODO: Add the ability to filter so that only certain beans are selected and run.
+		@SuppressWarnings("unchecked")
 		Map<String,FileRetriever> fileRetrievers = context.getBean("FileRetrievers", Map.class);
-		
-		for (String key : fileRetrievers.keySet())
-		{
-			FileRetriever retriever = fileRetrievers.get(key);
-			logger.info("Executing downloader: {}",key);
+		//Execute the file retreivers in parallel
+		fileRetrievers.keySet().stream().parallel().forEach(k -> {
+			FileRetriever retriever = fileRetrievers.get(k);
+			logger.info("Executing downloader: {}",k);
 			try
 			{
 				retriever.fetchData();
@@ -43,13 +45,29 @@ public class AddLinks {
 			catch (Exception e)
 			{
 				//TODO: The decision to continue after a failure should be a configurable option. 
-				logger.info("Exception caught while processing {}, message is: {}. Will continue with next file retriever.",key,e.getMessage());
-			}
-		}
+				logger.info("Exception caught while processing {}, message is: {}. Will continue with next file retriever.",k,e.getMessage());
+			}			
+		});
+		
+//		for (String key : fileRetrievers.keySet())
+//		{
+//			FileRetriever retriever = fileRetrievers.get(key);
+//			logger.info("Executing downloader: {}",key);
+//			try
+//			{
+//				retriever.fetchData();
+//			}
+//			catch (Exception e)
+//			{
+//				//TODO: The decision to continue after a failure should be a configurable option. 
+//				logger.info("Exception caught while processing {}, message is: {}. Will continue with next file retriever.",key,e.getMessage());
+//			}
+//		}
 		
 		//Now download mapping data from Uniprot.
 		ReferenceGeneProductCache.setDbParams("127.0.0.1", "test_reactome_57", "curator", "",3307);
-		
+		//TODO: Add the ability to filter so that only certain beans are selected and run.
+		@SuppressWarnings("unchecked")
 		Map<String,UniprotFileRetreiver> uniprotFileRetrievers = context.getBean("UniProtFileRetrievers", Map.class);
 		for (String key : uniprotFileRetrievers.keySet())
 		{
@@ -110,36 +128,44 @@ public class AddLinks {
 		
 		logger.info("Now processing the files...");
 
+		Map<String,Map<String,String>> dbMappings = new HashMap<String, Map<String,String>>();
+		
 		PROFileProcessor proFileProcessor = new PROFileProcessor();
 		proFileProcessor.setPath(Paths.get("/tmp/uniprotmapping.txt"));
 		Map<String,String> proMappings = proFileProcessor.getIdMappingsFromFile();
+		dbMappings.put("PRO", proMappings);
 
 		FlyBaseFileProcessor flyBaseFileProcessor = new FlyBaseFileProcessor();
 		flyBaseFileProcessor.setPath(Paths.get("/tmp/FlyBase.tsv.gz"));
-		Map<String,String> flyBaseMappings = flyBaseFileProcessor.getIdMappingsFromFile();		
+		Map<String,String> flyBaseMappings = flyBaseFileProcessor.getIdMappingsFromFile();
+		dbMappings.put("flyBase", flyBaseMappings);
 		
 		HmdbMetabolitesFileProcessor hmdbMetabolitesProcessor = new HmdbMetabolitesFileProcessor();
 		hmdbMetabolitesProcessor.setPath(Paths.get("/tmp/hmdb_metabolites.zip"));
 		Map<String,String> hmdbMetabolitesMappings = hmdbMetabolitesProcessor.getIdMappingsFromFile();
+		dbMappings.put("hmdbMetabolites", hmdbMetabolitesMappings);
 		
 		//...because it's the same code, just a different input file. right?
 //		HmdbMetabolitesFileProcessor hmdbProteinsProcessor = new HmdbMetabolitesFileProcessor();
 //		hmdbProteinsProcessor.setPath(Paths.get("/tmp/hmdb_proteins.zip"));
 //		Map<String,String> hmdbProteinsMappings = hmdbProteinsProcessor.getIdMappingsFromFile();
+//		dbMappings.put("hmdbProteins", hmdbProteinsMappings);
 		
 		OrphanetFileProcessor orphanetFileProcessor = new OrphanetFileProcessor();
 		orphanetFileProcessor.setPath(Paths.get("/tmp/genes_diseases_external_references.xml"));
 		Map<String,String> orphanetMappings = orphanetFileProcessor.getIdMappingsFromFile();
+		dbMappings.put("orphanet", orphanetMappings);
 		
 		IntActFileProcessor intactFileProcessor = new IntActFileProcessor();
 		intactFileProcessor.setPath(Paths.get("/tmp/reactome.dat"));
 		Map<String,String> intactFileMappings = intactFileProcessor.getIdMappingsFromFile();
+		dbMappings.put("intact", intactFileMappings);
 
 		//Only ZincMolecules needs a file processor. ZincProteins can be read straight from the file because it just a listing of IDs, one per line.
 		ZincMoleculesFileProcessor zincMoleculesFileProcessor = new ZincMoleculesFileProcessor();
 		zincMoleculesFileProcessor.setPath(Paths.get("/tmp/zinc_chebi_purch.xls"));
 		Map<String,String> zincMappings = zincMoleculesFileProcessor.getIdMappingsFromFile();
-
+		dbMappings.put("zinc", zincMappings);
 
 		logger.info("Process complete.");
 		context.close();
