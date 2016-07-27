72a2b171b733815711449869ae07dcac040a3afd
SolomonShorser-OICR <solomon.shorser@oicr.on.ca>
2016-07-27 16:42:55 -0400

    Got Ensembl query working and added test code for it.

--- src/main/java/org/reactome/addlinks/dataretrieval/EnsemblFileRetriever.java
+++ src/main/java/org/reactome/addlinks/dataretrieval/EnsemblFileRetriever.java
@@ -1,13 +1,17 @@
 package org.reactome.addlinks.dataretrieval;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
+import java.nio.file.OpenOption;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.apache.http.client.ClientProtocolException;
@@ -26,7 +30,9 @@ public class EnsemblFileRetriever extends FileRetriever
 
 	private String mapFromDb="";
 	private String mapToDb="";
-	private InputStream inStream;
+	private String species;
+	private List<String> identifiers;
+	//private InputStream inStream;
 
 	public enum EnsemblDB
 	{
@@ -98,31 +104,40 @@ public class EnsemblFileRetriever extends FileRetriever
 		this.mapToDb = mapToDb;
 	}
 	
-	public void setDataInputStream(InputStream inStream)
-	{
-		this.inStream = inStream;
-	}
-	
 	public String getFetchDestination()
 	{
 		return this.destination;
+	}
+	
+	public void setSpecies(String s)
+	{
+		this.species = s;
+	}
+	
+	public void setIdentifiers(List<String> identifiers)
+	{
+		this.identifiers = identifiers;
 	}
 	
 	@Override
 	public void downloadData()
 	{
 		// Check inputs:
-		if (this.inStream == null)
-		{
-			throw new RuntimeException("inStream is null! You must provide an data input stream!");
-		}
-		else if (this.mapFromDb.trim().length() == 0)
+		if (this.mapFromDb.trim().length() == 0)
 		{
 			throw new RuntimeException("You must provide a database name to map from!");
 		}
 		else if(this.mapToDb.trim().length() == 0)
 		{
 			throw new RuntimeException("You must provide a database name to map to!");
+		}
+		else if (this.species.trim().length() == 0)
+		{
+			throw new RuntimeException("You must provide a species name for the mapping!") ;
+		}
+		else if (this.identifiers == null || this.identifiers.isEmpty())
+		{
+			throw new RuntimeException("You must provide a list of identifiers to map!") ;
 		}
 		
 		
@@ -132,26 +147,42 @@ public class EnsemblFileRetriever extends FileRetriever
 			// http://rest.ensembl.org/xrefs/id/ENSOCUT00000003872?content-type=text/xml&all_levels=1&species=Oryctolagus_cuniculus&external_db=uniprot/swissprot
 			// or
 			// http://rest.ensembl.org/xrefs/id/ENSG00000175899?content-type=text/xml&all_levels=1&species=Homo_Sapiens&external_db=EntrezGene
-			
 			// ...then, extract all primary_id attributes. Be sure to get any synonyms as well (the EntrezGene URI demonstrates this). 
 
 			try
 			{
-				
-				URIBuilder builder = new URIBuilder(this.uri);
-				builder.addParameter("content-type", "text/xml")
-						.addParameter("all_levels", "1")
-						.addParameter("external_db", this.getMapToDb());
-				HttpGet get = new HttpGet(builder.build());
-				
-				try (CloseableHttpClient getClient = HttpClients.createDefault();
-						CloseableHttpResponse getResponse = getClient.execute(get);)
+				Path path = Paths.get(new URI("file://" + this.destination));
+				String responseContent = "";
+				for (String identifier : identifiers)
 				{
-					Path path = Paths.get(new URI("file://" + this.destination));
-					Files.createDirectories(path.getParent());
-					Files.write(path, EntityUtils.toByteArray(getResponse.getEntity()));
+					URIBuilder builder = new URIBuilder();
+
+					builder.setHost(this.uri.getHost())
+							.setPath(this.uri.getPath() + identifier)
+							.setScheme(this.uri.getScheme())
+							.addParameter("content-type", "text/xml")
+							.addParameter("all_levels", "1")
+							.addParameter("species", this.species)
+							.addParameter("external_db", this.getMapToDb());
+					HttpGet get = new HttpGet(builder.build());
+					logger.debug("URI: "+get.getURI());
+					try (CloseableHttpClient getClient = HttpClients.createDefault();
+							CloseableHttpResponse getResponse = getClient.execute(get);)
+					{
+						if (getResponse.getStatusLine().getStatusCode() == 500)
+						{
+							logger.error("Error 500 detected! Message: {}",getResponse.getStatusLine().getReasonPhrase());
+						}
+						else
+						{
+							String content = EntityUtils.toString(getResponse.getEntity());
+							//We'll store everything in an XML and then use a FileProcessor to sort out the details later.
+							responseContent += "<identifier id=\""+identifier+"\">\n"+content+"</identifier>\n";
+						}
+					}
 				}
-				
+				Files.createDirectories(path.getParent());
+				Files.write(path, responseContent.getBytes(), StandardOpenOption.CREATE);
 			}
 			catch (URISyntaxException e)
 			{
