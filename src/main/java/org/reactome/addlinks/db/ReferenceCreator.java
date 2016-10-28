8e9100c76f825d0db9f4083475fcc7f14812c7dc
SolomonShorser-OICR <solomon.shorser@oicr.on.ca>
2016-10-28 17:00:11 -0400

    Start re-writing this

--- src/main/java/org/reactome/addlinks/db/ReferenceCreator.java
+++ src/main/java/org/reactome/addlinks/db/ReferenceCreator.java
@@ -14,40 +14,24 @@ import org.gk.schema.InvalidAttributeException;
 import org.gk.schema.InvalidAttributeValueException;
 import org.gk.util.GKApplicationUtilities;
 
-public class IdentifierUpdater
+public class ReferenceCreator
 {
 	private static final Logger logger = LogManager.getLogger();
 	
 	Collection<GKInstance> identifiers;
 	
 	private MySQLAdaptor dbAdapter;
-	
-	public IdentifierUpdater(long id, MySQLAdaptor adapter)
-	{
-		this.dbAdapter = adapter;
-		
-		try
-		{
-			// When user provides a DB ID, there should only be one returned object.
-			this.identifiers.add( this.dbAdapter.fetchInstance(id) );
-		}
-		catch (Exception e)
-		{
-			logger.error("Could not get GKInstance for DatabaseIdentifier with DB ID: {}. Error was: {}", id, e.getMessage());
-			e.printStackTrace();
-		}
-	}
-	
 
 	
-	public IdentifierUpdater(String identifier, MySQLAdaptor adapter)
+	public ReferenceCreator(String identifier, MySQLAdaptor adapter)
 	{
 		this.dbAdapter = adapter;
-		GKSchemaAttribute atttribute = new GKSchemaAttribute();
-		atttribute.setName("Identifier");
+		GKSchemaAttribute attribute = new GKSchemaAttribute();
+		attribute.setName("Identifier");
 		try
 		{
-			this.identifiers = (Collection<GKInstance>) this.dbAdapter.fetchInstanceByAttribute(atttribute , "=", identifier);
+			//Try to see if this Identifier is already in the database.  Ideally, this will not return anything.
+			this.identifiers = (Collection<GKInstance>) this.dbAdapter.fetchInstanceByAttribute(attribute , "=", identifier);
 		}
 		catch (Exception e)
 		{
@@ -59,38 +43,80 @@ public class IdentifierUpdater
 	
 	public void updateIdentifierAttribute(String attribute, String value, long personID, String creatorName)
 	{
-		for (GKInstance identifier : this.identifiers)
+		// Before, purge all alt-db IDs so you have clean slate, make this purging optional? Or check for pre-existing alt-db IDs...
+		// For given uniprot ID, find referenced alt-db ID and if it already exists and matches given ref val, do nothing.
+		// other wise, create it. 
+		
+		if (this.identifiers != null && this.identifiers.size() > 0)
 		{
-			try
+			GKInstance instanceEdit = createInstanceEdit(personID, creatorName);
+			if (instanceEdit != null)
 			{
-				identifier.addAttributeValue(attribute, value);
-				// TODO: Also need to create an InstanceEdit to track the modifications.
-				
-				GKInstance instanceEdit = createDefaultIE(this.dbAdapter, personID, true);
-				
-				instanceEdit.getDBID();
-				instanceEdit.setAttributeValue(ReactomeJavaConstants.note, "crossReference inserted by " + creatorName);
-				this.dbAdapter.updateInstance(instanceEdit);
-				identifier.addAttributeValue(ReactomeJavaConstants.created, instanceEdit);
-				
-				this.dbAdapter.updateInstance(identifier);
-				
+				for (GKInstance identifier : this.identifiers)
+				{
+					try
+					{
+						identifier.addAttributeValue(attribute, value);
+						// TODO: Also need to create an InstanceEdit to track the modifications.					
+						
+						identifier.addAttributeValue(ReactomeJavaConstants.created, instanceEdit);
+						
+						this.dbAdapter.updateInstance(identifier);
+						
+					}
+					catch (InvalidAttributeException e)
+					{
+						logger.error("The attribte {} is invalid.", attribute);
+						e.printStackTrace();
+					}
+					catch (InvalidAttributeValueException e)
+					{
+						logger.error("The value \"{}\" given for the attribute \"{}\" is not a valid value.", attribute, value);
+						e.printStackTrace();
+					}
+					catch (Exception e)
+					{
+						e.printStackTrace();
+					}
+				}
 			}
-			catch (InvalidAttributeException e)
+			else
 			{
-				logger.error("The attribte {} is invalid.", attribute);
-				e.printStackTrace();
-			}
-			catch (InvalidAttributeValueException e)
-			{
-				logger.error("The value \"{}\" given for the attribute \"{}\" is not a valid value.", attribute, value);
-				e.printStackTrace();
-			}
-			catch (Exception e)
-			{
-				e.printStackTrace();
+				logger.error("InstanceEdit was null. Cannot create/update Identifiers without a valid InstanceEdit!");
+				// throw a runtime exception - this type of error should terminate the program.
+				throw new RuntimeException("Could not create an InstanceEdit. Aborting execution.");
 			}
 		}
+	}
+
+
+
+	private GKInstance createInstanceEdit(long personID, String creatorName)
+	{
+		GKInstance instanceEdit = null;
+		try
+		{
+			instanceEdit = createDefaultIE(this.dbAdapter, personID, true);
+			instanceEdit.getDBID();
+			instanceEdit.setAttributeValue(ReactomeJavaConstants.note, "crossReference inserted by " + creatorName);
+			this.dbAdapter.updateInstance(instanceEdit);
+		}
+		catch (InvalidAttributeException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		catch (InvalidAttributeValueException e)
+		{
+			e.printStackTrace();
+		}
+		catch (Exception e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return instanceEdit;
 	}
 	
 	private static GKInstance createDefaultIE(MySQLAdaptor dba, Long defaultPersonId, boolean needStore) throws Exception
