ccb5b82f16a063531b38f59a29bb6113fe71b670
SolomonShorser-OICR <solomon.shorser@oicr.on.ca>
2016-11-11 10:59:41 -0500

    New test: add new ReferenceDB with name matching existing REferenceDB

--- src/test/java/org/reactome/addlinks/test/TestReferenceDatabaseCreator.java
+++ src/test/java/org/reactome/addlinks/test/TestReferenceDatabaseCreator.java
@@ -88,4 +88,68 @@ public class TestReferenceDatabaseCreator
 		}
 		
 	}
+	
+	@Test
+	public void testPreexistingReferenceDatabaseCreation() throws SQLException, InvalidAttributeException, Exception
+	{
+		MySQLAdaptor adapter = null;
+		adapter = new MySQLAdaptor("localhost", "test_reactome_58","root","", 3306);
+		
+		String refDBName = "TestReferenceDatabase";
+		String refDBAlias = "TestRefDB";
+		String refDBUrl = "http://test.reference.database/";
+		String refDBAccessUrl = "http://test.reference.database/?##ID##";
+		SchemaAttribute refDBNameAttrib = adapter.getSchema().getClassByName(ReactomeJavaConstants.ReferenceDatabase).getAttribute(ReactomeJavaConstants.name);
+		Collection<GKInstance> instances = adapter.fetchInstanceByAttribute(refDBNameAttrib, "=", refDBName);
+		
+		//This test should clean itself up, but in case it doesn't work, delete the instances if they DO exist.
+		if (instances != null)
+		{
+			for (GKInstance instance : instances)
+			{
+				System.out.println("Deleting pre-existing instance: "+instance.getDBID()+" "+instance.getDisplayName());
+				adapter.deleteInstance(instance);
+			}
+		}
+		
+		ReferenceDatabaseCreator creator = new ReferenceDatabaseCreator(adapter);
+		creator.createReferenceDatabase(refDBUrl, refDBAccessUrl, refDBName, refDBAlias);
+		
+		//Now, let's try to create another ReferenceDatabase, with one name that matches the first one.
+		String refDBAlias2 = "Test_Ref_DB_2";
+		creator.createReferenceDatabase(refDBUrl, refDBAccessUrl, refDBName, refDBAlias2);
+		
+		instances = adapter.fetchInstanceByAttribute(refDBNameAttrib, "=", refDBName);
+		assertTrue(instances.size() == 1);
+		instances.forEach( instance -> {
+			System.out.println("Checking instance: "+instance.getDBID()+" "+instance.getDisplayName());
+			try
+			{
+				assertEquals(instance.getAttributeValue(ReactomeJavaConstants.url),refDBUrl);
+				assertEquals(instance.getAttributeValue(ReactomeJavaConstants.accessUrl),refDBAccessUrl);
+				Collection<String> names = instance.getAttributeValuesList(ReactomeJavaConstants.name);
+				assertTrue(names.size() == 3);
+				for (String name : names)
+				{
+					System.out.println("checking name: "+name);
+					assertTrue(name.equals(refDBName) || name.equals(refDBAlias) || name.equals(refDBAlias2));
+				}
+			}
+			catch (InvalidAttributeException e)
+			{
+				e.printStackTrace();
+				fail();
+			}
+			catch (Exception e)
+			{
+				e.printStackTrace();
+				fail();
+			}
+		});
+		for (GKInstance instance : instances)
+		{
+			// If everything was OK, let's delete the instance (don't want to clutter up the database.
+			adapter.deleteInstance(instance);
+		}
+	}
 }
