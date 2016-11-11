77a9d644692dd16345f1263549186e51f5c4e00b
SolomonShorser-OICR <solomon.shorser@oicr.on.ca>
2016-11-11 10:47:01 -0500

    New testing class.

---
+++ src/test/java/org/reactome/addlinks/test/TestReferenceDatabaseCreator.java
@@ -0,0 +1,91 @@
+package org.reactome.addlinks.test;
+
+import static org.junit.Assert.*;
+
+import java.sql.SQLException;
+import java.util.Collection;
+
+import org.gk.model.GKInstance;
+import org.gk.model.ReactomeJavaConstants;
+import org.gk.persistence.MySQLAdaptor;
+import org.gk.schema.InvalidAttributeException;
+import org.gk.schema.SchemaAttribute;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.PowerMockIgnore;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+import org.reactome.addlinks.db.ReferenceDatabaseCreator;
+
+@PowerMockIgnore({"javax.management.*","javax.net.ssl.*"})
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({ java.net.URI.class,
+				org.apache.commons.net.ftp.FTPClient.class,
+				org.reactome.addlinks.db.ReferenceDatabaseCreator.class,
+				org.apache.http.impl.client.HttpClients.class })
+public class TestReferenceDatabaseCreator
+{
+
+	
+	@Test
+	public void testCreateSimpleReferenceDatabase() throws SQLException, InvalidAttributeException, Exception
+	{
+		MySQLAdaptor adapter = null;
+		adapter = new MySQLAdaptor("localhost", "test_reactome_58","root","", 3306);
+		
+		String refDBName = "TestReferenceDatabase";
+		String refDBAlias = "TestRefDB";
+		String refDBUrl = "http://test.reference.database/";
+		String refDBAccessUrl = "http://test.reference.database/?##ID##";
+		
+		SchemaAttribute refDBNameAttrib = adapter.getSchema().getClassByName(ReactomeJavaConstants.ReferenceDatabase).getAttribute(ReactomeJavaConstants.name);
+		Collection<GKInstance> instances = adapter.fetchInstanceByAttribute(refDBNameAttrib, "=", refDBName);
+		
+		//This test should clean itself up, but in case it doesn't work, delete the instances if they DO exist.
+		if (instances != null)
+		{
+			for (GKInstance instance : instances)
+			{
+				System.out.println("Deleting pre-existing instance: "+instance.getDBID()+" "+instance.getDisplayName());
+				adapter.deleteInstance(instance);
+			}
+		}
+		
+		ReferenceDatabaseCreator creator = new ReferenceDatabaseCreator(adapter);
+		creator.createReferenceDatabase(refDBUrl, refDBAccessUrl, refDBName, refDBAlias);
+		// Now that we've created the instance, let's verify that it is there.
+		instances = adapter.fetchInstanceByAttribute(refDBNameAttrib, "=", refDBName);
+		assertTrue(instances.size() == 1);
+		instances.forEach( instance -> {
+			System.out.println("Checking instance: "+instance.getDBID()+" "+instance.getDisplayName());
+			try
+			{
+				assertEquals(instance.getAttributeValue(ReactomeJavaConstants.url),refDBUrl);
+				assertEquals(instance.getAttributeValue(ReactomeJavaConstants.accessUrl),refDBAccessUrl);
+				Collection<String> names = instance.getAttributeValuesList(ReactomeJavaConstants.name);
+				assertTrue(names.size() == 2);
+				for (String name : names)
+				{
+					System.out.println("checking name: "+name);
+					assertTrue(name.equals(refDBName) || name.equals(refDBAlias));
+				}
+			}
+			catch (InvalidAttributeException e)
+			{
+				e.printStackTrace();
+				fail();
+			}
+			catch (Exception e)
+			{
+				e.printStackTrace();
+				fail();
+			}
+		});
+		for (GKInstance instance : instances)
+		{
+			// If everything was OK, let's delete the instance (don't want to clutter up the database.
+			adapter.deleteInstance(instance);
+		}
+		
+	}
+}
