100b24b48c4bbf76d47175087f0efdb430190049
SolomonShorser-OICR <solomon.shorser@oicr.on.ca>
2016-11-11 10:26:21 -0500

    Restructured to code to update any pre-existing ReferenceDatabases

--- src/main/java/org/reactome/addlinks/db/ReferenceDatabaseCreator.java
+++ src/main/java/org/reactome/addlinks/db/ReferenceDatabaseCreator.java
@@ -1,8 +1,10 @@
 package org.reactome.addlinks.db;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -10,7 +12,6 @@ import org.gk.model.GKInstance;
 import org.gk.model.InstanceDisplayNameGenerator;
 import org.gk.model.ReactomeJavaConstants;
 import org.gk.persistence.MySQLAdaptor;
-import org.gk.persistence.MySQLAdaptor.QueryRequest;
 import org.gk.schema.SchemaAttribute;
 import org.gk.schema.SchemaClass;
 
@@ -24,7 +25,16 @@ public class ReferenceDatabaseCreator
 		this.adapter = adapter;
 	}
 	
-	public void createReferenceDatabase(String url, String accessUrl, String ... names)
+	/**
+	 * Creates a ReferenceDatabase. If the ReferenceDatabase already exists, it will not be created.
+	 * @param url - The URL of the ReferenceDatabase.
+	 * @param accessUrl - The access URL of the RefereneDatabase.
+	 * @param names - A list of names for this ReferenceDatabase. If none of these values are already in the database, then a new ReferenceDatabase
+	 * will be created and these names will be associated with it. If any of these names already exist, and the other names will be associated with it.
+	 * The URLs will not be updated in the case that a ReferenceDatabase name is pre-existing in the database.
+	 * @throws Exception 
+	 */
+	public void createReferenceDatabase(String url, String accessUrl, String ... names) throws Exception
 	{
 		//First, let's check that the Reference Database doesn't already exist. all we have to go on is the name...
 		SchemaClass refDBClass = adapter.getSchema().getClassByName(ReactomeJavaConstants.ReferenceDatabase);
@@ -32,43 +42,65 @@ public class ReferenceDatabaseCreator
 		try
 		{
 			SchemaAttribute dbNameAttrib = refDBClass.getAttribute(ReactomeJavaConstants.name);
-			GKInstance newReferenceDB = null ;
+			
+			List<String> namesNotYetInDB = new ArrayList<String>(names.length);
+			List<GKInstance> instancesInDB = new ArrayList<GKInstance>();
 			for (String name : names)
 			{
 				// This is tricky: If there is an existing ReferenceDatabase that has a name match, we should probably ADD the other names to that ReferenceDatabase,
 				// Rather than create new ones. Unless the URLs don't match then maybe we should create new ReferenceDatabases? This needsa bit more thought.
 				Collection<GKInstance> preexistingReferenceDBs =  adapter.fetchInstanceByAttribute(dbNameAttrib, "=", name);
 				
-				// Only proceed if there are no other databases with the same name.
+				// Add to the list if the name is not yet in the database.
 				if (preexistingReferenceDBs.size() == 0)
 				{
-					// Only create the ReferenceDatabase object for the first name we see.
-					if (newReferenceDB == null)
-					{
-						newReferenceDB = new GKInstance(refDBClass);
-					}
-					// A reference database could have multiple names/aliases, so keep adding them.
-					newReferenceDB.addAttributeValue(dbNameAttrib, name);
-					// There should only be one url and accessUrl so SET the attribute - it's OK to set the same attribute to the same value multiple times in this loop.
-					newReferenceDB.setAttributeValue(ReactomeJavaConstants.url, url);
-					newReferenceDB.setAttributeValue(ReactomeJavaConstants.accessUrl, url);
+					namesNotYetInDB.add(name);
 				}
 				else
 				{
 					logger.info("It looks like there were already {} Reference Databases with the name {}, so no new Reference Databases with this name will be created.", preexistingReferenceDBs.size(), name);
+					// I really don't expect preexistingReferenceDBs to have more than 1 element, but just in case it does...
+					instancesInDB.addAll(preexistingReferenceDBs);
 				}
 			}
-			// If the ReferenceDatabase object actually got created, we need to store it.
-			if (newReferenceDB != null)
+			
+			//No RefDatabases that match any of the given names exist, so we're creating the Reference Database with all names now. 
+			if (instancesInDB.size() == 0)
 			{
+				GKInstance newReferenceDB = new GKInstance(refDBClass);
+				// A reference database could have multiple names/aliases, so keep adding them.
+				for (String name : names)
+				{
+					newReferenceDB.addAttributeValue(dbNameAttrib, name);
+				}
+
+				newReferenceDB.setAttributeValue(ReactomeJavaConstants.url, url);
+				newReferenceDB.setAttributeValue(ReactomeJavaConstants.accessUrl, url);
 				InstanceDisplayNameGenerator.setDisplayName(newReferenceDB);
 				adapter.storeInstance(newReferenceDB);
 			}
+			//Othwerwise, some ReferenceDatabase object(s) already exist with some of the names given here. So, we need to update it with the new names. 
+			else
+			{
+				for (GKInstance preexistingRefDB : instancesInDB)
+				{
+					List<String> preexistingNames = (List<String>)preexistingRefDB.getAttributeValuesList(dbNameAttrib);
+					// the names to add: everything that is in the input parameter "names" but not in "preexistingNames"
+					List<String> namesToAdd = (Arrays.asList(names)).stream().filter(p -> !preexistingNames.contains(p)).collect(Collectors.toList());
+					
+					for (String n : namesToAdd)
+					{
+						preexistingRefDB.addAttributeValue(dbNameAttrib, n);
+						this.adapter.updateInstanceAttribute(preexistingRefDB, dbNameAttrib);
+					}
+				}
+			}
+
 		}
 		catch (Exception e)
 		{
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			logger.error("Error while trying to create a Reference Dataabse object: "+e.getMessage());
+			throw e;
 		}
 	}
 }
