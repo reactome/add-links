ef97b71705563544a1d0851f459fc3299bb352e7
SolomonShorser-OICR <solomon.shorser@oicr.on.ca>
2016-11-22 12:00:54 -0500

    More code changes to use GKInstance+API instead of raw SQL.

--- src/test/java/org/reactome/addlinks/test/TestReferenceGeneProductCache.java
+++ src/test/java/org/reactome/addlinks/test/TestReferenceGeneProductCache.java
@@ -1,28 +1,41 @@
 package org.reactome.addlinks.test;
 
 
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 import java.util.List;
 
 import org.gk.model.GKInstance;
 import org.gk.model.ReactomeJavaConstants;
+import org.gk.persistence.MySQLAdaptor;
 import org.gk.schema.InvalidAttributeException;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.reactome.addlinks.db.ReferenceObjectCache;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
 
+
+@ContextConfiguration("/test-application-context.xml")
+@RunWith(org.springframework.test.context.junit4.SpringJUnit4ClassRunner.class)
 public class TestReferenceGeneProductCache {
+	
+	// This adapter will be populated with the adapter in the Spring config file.
+	@Autowired
+	MySQLAdaptor adapter;
+	
+
 	
 	@Test
 	public void testCacheBySpecies()
 	{
-		ReferenceObjectCache cache;
+		
 		
 		//TODO: all test classes really should get their adapter froma spring file or something.
-		ReferenceObjectCache.setDbParams("127.0.0.1", "test_reactome_57", "curator", "",3307);
+		//ReferenceObjectCache.setDbParams("127.0.0.1", "test_reactome_57", "curator", "",3307);
+		ReferenceObjectCache.setAdapter(adapter);
 		
-		cache = ReferenceObjectCache.getInstance();
+		ReferenceObjectCache cache = ReferenceObjectCache.getInstance();
 		
 		List <GKInstance> items = cache.getBySpecies("48887");
 		
@@ -35,28 +48,30 @@ public class TestReferenceGeneProductCache {
 	@Test
 	public void testCacheById() throws InvalidAttributeException, Exception
 	{
-		ReferenceObjectCache cache;
+		//ReferenceObjectCache cache;
 		
-		ReferenceObjectCache.setDbParams("127.0.0.1", "test_reactome_57", "curator", "",3307);
+		//ReferenceObjectCache.setDbParams("127.0.0.1", "test_reactome_57", "curator", "",3307);
+		ReferenceObjectCache.setAdapter(adapter);
 		
-		cache = ReferenceObjectCache.getInstance();
+		ReferenceObjectCache cache = ReferenceObjectCache.getInstance();
 		
 		GKInstance shell = cache.getById("5618411");
-		
+
 		assertNotNull(shell);
-		assertTrue("5618411".equals(shell.getDBID()));
-		assertTrue("2".equals(shell.getAttributeValue(ReactomeJavaConstants.referenceDatabase)));
-		assertTrue("48887".equals(shell.getAttributeValue(ReactomeJavaConstants.species)));
-		assertTrue("P0DML2".equals(shell.getAttributeValue(ReactomeJavaConstants.identifier)));
-		assertTrue("UniProt:P0DML2 CSH1".equals(shell.getDisplayName()));
+		assertTrue(5618411 == shell.getDBID());
+		assertTrue(2 == ((GKInstance)shell.getAttributeValue(ReactomeJavaConstants.referenceDatabase)).getDBID()  );
+		assertTrue(48887 == ((GKInstance)shell.getAttributeValue(ReactomeJavaConstants.species)).getDBID() );
+		assertEquals("UniProt:P0DML2 CSH1",(shell.getDisplayName()));
+		assertEquals("P0DML2",(  shell.getAttributeValue(ReactomeJavaConstants.identifier) ));
 	}
 	
 	@Test
 	public void testCacheByRefDbId()
 	{
-		ReferenceObjectCache cache;
-		ReferenceObjectCache.setDbParams("127.0.0.1", "test_reactome_57", "curator", "",3307);
-		cache = ReferenceObjectCache.getInstance();
+		//ReferenceObjectCache cache;
+		//ReferenceObjectCache.setDbParams("127.0.0.1", "test_reactome_57", "curator", "",3307);
+		ReferenceObjectCache.setAdapter(adapter);
+		ReferenceObjectCache cache = ReferenceObjectCache.getInstance();
 		List <GKInstance> items = cache.getByRefDb("427877");
 		
 		assertNotNull(items);
@@ -68,9 +83,10 @@ public class TestReferenceGeneProductCache {
 	@Test
 	public void testCacheByRefDbAndId()
 	{
-		ReferenceObjectCache cache;
-		ReferenceObjectCache.setDbParams("127.0.0.1", "test_reactome_57", "curator", "",3307);
-		cache = ReferenceObjectCache.getInstance();
+		//ReferenceObjectCache cache;
+		//ReferenceObjectCache.setDbParams("127.0.0.1", "test_reactome_57", "curator", "",3307);
+		ReferenceObjectCache.setAdapter(adapter);
+		ReferenceObjectCache cache = ReferenceObjectCache.getInstance();
 		List <GKInstance> items = cache.getByRefDbAndSpecies("2", "48898");
 		
 		assertNotNull(items);
