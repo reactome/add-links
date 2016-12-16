1fcb6ee54dca9c0fec20d13b17e220d5cf6e28c8
SolomonShorser-OICR <solomon.shorser@oicr.on.ca>
2016-12-16 11:36:33 -0500

    HMDB Molecules XML file process wasn't quite right. Fixed it.
    The file processor was not properly mapping the values. Now, it maps an
    HMDB Id to nest structures continaing UniProt and ChEBI values.

--- src/test/java/org/reactome/addlinks/test/TestHMDB.java
+++ src/test/java/org/reactome/addlinks/test/TestHMDB.java
@@ -2,6 +2,7 @@ package org.reactome.addlinks.test;
 
 import static org.junit.Assert.*;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
@@ -10,7 +11,9 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.reactome.addlinks.dataretrieval.FileRetriever;
 import org.reactome.addlinks.db.ReferenceObjectCache;
+import org.reactome.addlinks.fileprocessors.HmdbMetabolitesFileProcessor;
 import org.reactome.addlinks.fileprocessors.HmdbProteinsFileProcessor;
+import org.reactome.addlinks.referencecreators.HMDBMoleculeReferenceCreator;
 import org.reactome.addlinks.referencecreators.HMDBProteinReferenceCreator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
@@ -31,16 +34,43 @@ public class TestHMDB
 	FileRetriever HmdbProteinsRetriever;
 	
 	@Autowired
+	FileRetriever HmdbMoleculesRetriever;
+	
+	@Autowired
 	HmdbProteinsFileProcessor HmdbProteinsFileProcessor;
 
 	@Autowired
 	HMDBProteinReferenceCreator HMDBProtReferenceCreator;
+	
+	@Autowired
+	HmdbMetabolitesFileProcessor HmdbMoleculesFileProcessor;
+	
+//	@Autowired
+//	HMDBMoleculeReferenceCreator HmdbMoleculeReferenceCreator;
 	
 	@Test
 	public void testGetHMDBProteinsFile() throws Exception
 	{
 		HmdbProteinsRetriever.fetchData();
 	}
+
+	@Test
+	public void testGetHMDBMoleculessFile() throws Exception
+	{
+		HmdbMoleculesRetriever.fetchData();
+	}
+	
+	@Test
+	public void testProcessHMDBMoleculesFile() throws Exception
+	{
+		HmdbMoleculesRetriever.fetchData();
+		
+		Map<String, Map<String, Collection<String>>> mappings = HmdbMoleculesFileProcessor.getIdMappingsFromFile();
+		
+		assertNotNull(mappings);
+		assertTrue(mappings.keySet().size() > 0);
+	}
+	
 	
 	@Test
 	public void testProcessHMDBProteinsFile() throws Exception
@@ -54,7 +84,7 @@ public class TestHMDB
 	}
 	
 	@Test
-	public void testCreateHMDBReferences() throws Exception
+	public void testCreateHMDBProteinsReferences() throws Exception
 	{
 		HmdbProteinsRetriever.fetchData();
 		
@@ -63,11 +93,28 @@ public class TestHMDB
 		assertNotNull(mappings);
 		assertTrue(mappings.keySet().size() > 0);
 		
-		String refDb = "2";
+		String refDB = objectCache.getRefDbNamesToIds().get("UniProt").get(0);
 		String species = "48887";
 		String className = "ReferenceGeneProduct";
-		List<GKInstance> sourceReferences = objectCache.getByRefDbAndSpecies(refDb, species, className);
+		List<GKInstance> sourceReferences = objectCache.getByRefDbAndSpecies(refDB, species, className);
 		
 		HMDBProtReferenceCreator.createIdentifiers(123456, mappings, sourceReferences);
 	}
+	
+//	@Test
+//	public void testCreateHMDBMoleculesReferences() throws Exception
+//	{
+//		HmdbMoleculesRetriever.fetchData();
+//		
+//		Map<String, String> mappings = HmdbMoleculesFileProcessor.getIdMappingsFromFile();
+//		
+//		assertNotNull(mappings);
+//		assertTrue(mappings.keySet().size() > 0);
+//		String refDB = objectCache.getRefDbNamesToIds().get("ChEBI").get(0);
+//		String species = "48887";
+//		String className = "ReferenceMolecule";
+//		List<GKInstance> sourceReferences = objectCache.getByRefDbAndSpecies(refDB, species, className);
+//		
+//		HmdbMoleculeReferenceCreator.createIdentifiers(123456, mappings, sourceReferences);
+//	}
 }
