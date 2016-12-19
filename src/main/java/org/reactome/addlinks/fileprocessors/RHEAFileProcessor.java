359db29cf2061ebb1d08da55aabfddfa4823bbc4
SolomonShorser-OICR <solomon.shorser@oicr.on.ca>
2016-12-19 17:32:09 -0500

    Will probably change the RHEA process
    The process will probably need to change to search RHEA for ChEBI IDs
    and then link the ChEBI IDs in Reactome to the RHEA IDs that are found.`

--- src/main/java/org/reactome/addlinks/fileprocessors/RHEAFileProcessor.java
+++ src/main/java/org/reactome/addlinks/fileprocessors/RHEAFileProcessor.java
@@ -17,11 +17,20 @@ import org.apache.logging.log4j.Logger;
 import org.w3c.dom.NodeList;
 import org.xml.sax.InputSource;
 
+
+/*
+ * Better way to do  all this: Use ChEBI IDs and send THOSE to RHEA service and create cross-ref links based on ChEBI-to-RHEA mappings.
+ * You can query directly for CHEBI IDs like this:  http://www.ebi.ac.uk/rhea/rest/1.0/ws/reaction?q=chebi:57444
+ * That will return a list of RHEA IDs which you could link to the ChEBI IDs or look for REACTOME identifiers and link to those as well.
+ */
+
 public class RHEAFileProcessor extends FileProcessor<String>
 {
 	private static final Logger logger = LogManager.getLogger();
 	// XPATH expression to get RHEA URIs: /ns2:RheaWsResponse/resultset/rheaReaction/rheaid/rheaUri/uri[../uriresponseformat/text()='cmlreact' ]
 	private static XPathExpression rheaURIPath ;
+	
+	// This XPATH expression will 
 	
 	static
 	{
@@ -71,8 +80,14 @@ public class RHEAFileProcessor extends FileProcessor<String>
 			for (int i = 0 ; i < nodeList.getLength() ; i ++)
 			{
 				String rheaURI = nodeList.item(i).getTextContent();
-				//Once we have the RHEA ID, we need to access it and parse the response for the Reactome ID.
+				// Once we have the RHEA ID, we need to access it and parse the response for the Reactome ID.
+				// The URIs will look like http://www.ebi.ac.uk/rhea/rest/1.0/ws/reaction/cmlreact/$rhea_id
+				// Remove everything before the ID to extract it
+				String rheaID = rheaURI.replaceAll("http://www\\.ebi\\.ac\\.uk/rhea/rest/1\\.0/ws/reaction/cmlreact/", "");
 				
+				// This XPath expression will extract the "link" elements that contain a "reactome.org" href attribute.
+				// This should be evaluated against the web-service response from querying a specific RHEA ID.
+				// /*[local-name()='reaction']/*[local-name()='map']/*[local-name()='link' and contains(./@href,'reactome.org')!='' ]
 			}
 		}
 		
