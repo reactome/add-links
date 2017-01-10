ed311fbd1b0ca75ce068651502bc3cf5e3564739
SolomonShorser-OICR <solomon.shorser@oicr.on.ca>
2017-01-10 17:40:09 -0500

    Test code: look up ENSP and get ENST ; lookup ENST and get ENSG.

--- src/test/java/org/reactome/addlinks/test/TestEnsemblFileRetrievalIT.java
+++ src/test/java/org/reactome/addlinks/test/TestEnsemblFileRetrievalIT.java
@@ -6,6 +6,7 @@ import static org.junit.Assert.assertTrue;
 import java.net.URI;
 import java.nio.file.Paths;
 import java.time.Duration;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -64,5 +65,50 @@ public class TestEnsemblFileRetrievalIT
 		Map<String,String> mappings = processor.getIdMappingsFromFile();
 		assertNotNull(mappings);
 		assertTrue(mappings.keySet().size() > 0);
+	}
+	
+	@Test
+	public void testEnsemblProteinToGeneLookup() throws Exception
+	{
+		String refDb = "ENSEMBL_Gallus gallus_PROTEIN";
+		String species = "Gallus gallus";
+		String className = "ReferenceGeneProduct";
+		List<String> refDBIDs = objectCache.getRefDbNamesToIds().get(refDb);
+		String refDBID = refDBIDs.get(0);
+		String speciesDBID = objectCache.getSpeciesNamesToIds().get(species).get(0);
+
+		List<String> identifiers = TestUtils.getIdentifiersList(refDBID,species,className,objectCache);
+		System.out.println("# identifiers: "+identifiers.size());
+		EnsemblBatchLookup protein2TranscriptRetriever = new EnsemblBatchLookup();		
+		protein2TranscriptRetriever.setSpecies(species.replace(" ", "_"));
+		//retriever.setDataURL(new URI("http://rest.ensembl.org/lookup/id/"));
+		protein2TranscriptRetriever.setDataURL(new URI("http://localhost:9999/lookup/id/"));
+		protein2TranscriptRetriever.setFetchDestination("/tmp/addlinks-downloaded-files/ensembl/ENSP_batch_lookup."+speciesDBID+"."+refDBID+".xml");
+		protein2TranscriptRetriever.setMaxAge(Duration.ZERO);
+		protein2TranscriptRetriever.setIdentifiers(identifiers);
+		protein2TranscriptRetriever.fetchData();
+		
+		EnsemblBatchLookupFileProcessor processor = new EnsemblBatchLookupFileProcessor();
+		
+		processor.setPath(Paths.get("/tmp/addlinks-downloaded-files/ensembl/ENSP_batch_lookup."+speciesDBID+"."+refDBID+".xml"));
+		Map<String,String> mappings = processor.getIdMappingsFromFile();
+		assertNotNull(mappings);
+		assertTrue(mappings.keySet().size() > 0);
+		
+		List<String> transcripts = new ArrayList<String>(mappings.values());
+		assertTrue(transcripts.size() > 0);
+		EnsemblBatchLookup transacript2GeneRetriever = new EnsemblBatchLookup();		
+		transacript2GeneRetriever.setSpecies(species.replace(" ", "_"));
+		//retriever.setDataURL(new URI("http://rest.ensembl.org/lookup/id/"));
+		transacript2GeneRetriever.setDataURL(new URI("http://localhost:9999/lookup/id/"));
+		transacript2GeneRetriever.setFetchDestination("/tmp/addlinks-downloaded-files/ensembl/ENST_batch_lookup."+speciesDBID+"."+refDBID+".xml");
+		transacript2GeneRetriever.setMaxAge(Duration.ZERO);
+		transacript2GeneRetriever.setIdentifiers(transcripts);
+		transacript2GeneRetriever.fetchData();
+		
+		processor.setPath(Paths.get("/tmp/addlinks-downloaded-files/ensembl/ENST_batch_lookup."+speciesDBID+"."+refDBID+".xml"));
+		Map<String,String> mappings2 = processor.getIdMappingsFromFile();
+		assertNotNull(mappings2);
+		assertTrue(mappings2.keySet().size() > 0);
 	}
 }
