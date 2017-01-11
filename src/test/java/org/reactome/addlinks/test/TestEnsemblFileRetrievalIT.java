78188a85f72423a9d10bf6a28636b88834e52a31
SolomonShorser-OICR <solomon.shorser@oicr.on.ca>
2017-01-11 17:45:11 -0500

    Add xref lookup to Ensembl IT ; improve error checking ;

--- src/test/java/org/reactome/addlinks/test/TestEnsemblFileRetrievalIT.java
+++ src/test/java/org/reactome/addlinks/test/TestEnsemblFileRetrievalIT.java
@@ -13,6 +13,8 @@ import java.util.Map;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.reactome.addlinks.dataretrieval.ensembl.EnsemblBatchLookup;
+import org.reactome.addlinks.dataretrieval.ensembl.EnsemblFileRetriever;
+import org.reactome.addlinks.dataretrieval.ensembl.EnsemblFileRetriever.EnsemblDB;
 import org.reactome.addlinks.db.ReferenceObjectCache;
 import org.reactome.addlinks.fileprocessors.ensembl.EnsemblBatchLookupFileProcessor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -110,5 +112,64 @@ public class TestEnsemblFileRetrievalIT
 		Map<String,String> mappings2 = processor.getIdMappingsFromFile();
 		assertNotNull(mappings2);
 		assertTrue(mappings2.keySet().size() > 0);
+		
+	}
+	
+	@Test public void testEnsemblXRefLookup() throws Exception
+	{
+		String refDb = "ENSEMBL_Gallus gallus_PROTEIN";
+		String species = "Gallus gallus";
+		String className = "ReferenceGeneProduct";
+		List<String> refDBIDs = objectCache.getRefDbNamesToIds().get(refDb);
+		String refDBID = refDBIDs.get(0);
+		String speciesDBID = objectCache.getSpeciesNamesToIds().get(species).get(0);
+
+		List<String> identifiers = TestUtils.getIdentifiersList(refDBID,species,className,objectCache);
+		System.out.println("# identifiers: "+identifiers.size());
+		EnsemblBatchLookup protein2TranscriptRetriever = new EnsemblBatchLookup();		
+		protein2TranscriptRetriever.setSpecies(species.replace(" ", "_"));
+		//retriever.setDataURL(new URI("http://rest.ensembl.org/lookup/id/"));
+		protein2TranscriptRetriever.setDataURL(new URI("http://localhost:9999/lookup/id/"));
+		protein2TranscriptRetriever.setFetchDestination("/tmp/addlinks-downloaded-files/ensembl/ENSP_batch_lookup."+speciesDBID+"."+refDBID+".xml");
+		protein2TranscriptRetriever.setMaxAge(Duration.ZERO);
+		protein2TranscriptRetriever.setIdentifiers(identifiers);
+		protein2TranscriptRetriever.fetchData();
+		
+		EnsemblBatchLookupFileProcessor processor = new EnsemblBatchLookupFileProcessor();
+		
+		processor.setPath(Paths.get("/tmp/addlinks-downloaded-files/ensembl/ENSP_batch_lookup."+speciesDBID+"."+refDBID+".xml"));
+		Map<String,String> mappings = processor.getIdMappingsFromFile();
+		assertNotNull(mappings);
+		assertTrue(mappings.keySet().size() > 0);
+		
+		List<String> transcripts = new ArrayList<String>(mappings.values());
+		assertTrue(transcripts.size() > 0);
+		EnsemblBatchLookup transacript2GeneRetriever = new EnsemblBatchLookup();		
+		transacript2GeneRetriever.setSpecies(species.replace(" ", "_"));
+		//retriever.setDataURL(new URI("http://rest.ensembl.org/lookup/id/"));
+		transacript2GeneRetriever.setDataURL(new URI("http://localhost:9999/lookup/id/"));
+		transacript2GeneRetriever.setFetchDestination("/tmp/addlinks-downloaded-files/ensembl/ENST_batch_lookup."+speciesDBID+"."+refDBID+".xml");
+		transacript2GeneRetriever.setMaxAge(Duration.ZERO);
+		transacript2GeneRetriever.setIdentifiers(transcripts);
+		transacript2GeneRetriever.fetchData();
+		
+		processor.setPath(Paths.get("/tmp/addlinks-downloaded-files/ensembl/ENST_batch_lookup."+speciesDBID+"."+refDBID+".xml"));
+		Map<String,String> mappings2 = processor.getIdMappingsFromFile();
+		assertNotNull(mappings2);
+		assertTrue(mappings2.keySet().size() > 0);
+		
+		EnsemblFileRetriever xrefRetriever = new EnsemblFileRetriever();
+		xrefRetriever.setDataURL(new URI("http://rest.ensembl.org/xrefs/id/"));
+		xrefRetriever.setFetchDestination("/tmp/addlinks-downloaded-files/ensembl-mappings/ensembl_to_EntrezGene.xml");
+		xrefRetriever.setMapFromDbEnum(EnsemblDB.ENSEMBLProtein);
+		xrefRetriever.setMapToDbEnum(EnsemblDB.EntrezGene);
+		List<String> ensgList = new ArrayList<String>(mappings2.values());
+		xrefRetriever.setIdentifiers(ensgList);
+		xrefRetriever.setSpecies(species.replace(" ", "_"));
+		xrefRetriever.setMaxAge(Duration.ZERO);
+		
+		
+		xrefRetriever.fetchData();
+		
 	}
 }
