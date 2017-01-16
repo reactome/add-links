bfd47f1340c1b6f837d51a23f85bbc0f6d4d138a
SolomonShorser-OICR <solomon.shorser@oicr.on.ca>
2017-01-16 17:01:22 -0500

    Mostly just notes but I want them committed for reference.

--- src/main/java/org/reactome/addlinks/referencecreators/IntActReferenceCreator.java
+++ src/main/java/org/reactome/addlinks/referencecreators/IntActReferenceCreator.java
@@ -31,7 +31,19 @@ public class IntActReferenceCreator extends SimpleReferenceCreator<String>
 				// If the Reactome value in the mapping file is a legitimate StableIdentifier, then proceed.
 				if (instance!=null)
 				{
-					// create the references....
+					// References will be created in Complex_2_interactionIdentifier, referencing DatabasIdentifier.
+					// From an object point-of-view, we will be adding DatabaseIdentifiers as interactionIdentifiers to Complexes.
+					this.refCreator.createIdentifier(intActID, String.valueOf(instance.getDBID()), this.getTargetRefDB(), personID, this.getClass().getName(), (Long)instance.getAttributeValue(ReactomeJavaConstants.species));
+					
+					// should we include a "generate interactions" flag like the old AddLinks, or just do it? For now, just do it!
+					
+					// Next: find interactions with ReferenceGeneProducts, those should be in sourceReferences, should be all UniProt IDs
+					// I think this is what needs to happen:
+					// Go through ALL UniProt references in the database.
+					// If a UniProt ID has a StableIdentifier that is in "mappings" (as a value) that means it is in an interaction.
+					// In this case, find PhysicalEntites the refer to the ReferenceEntitythat the UniProt ID refers to.
+					
+					
 				}
 				// The old code would try to do a lookup to see if mappings.get(intActID) was a db_id instead of a stable identifier.
 				// The data file doesn't seem to contain ANY DB_IDs so that's probably just some old legacy stuff. 
