ebcc2e69a77c8423c35833261d57add9ba6f6918
SolomonShorser-OICR <solomon.shorser@oicr.on.ca>
2017-01-31 15:39:50 -0500

    Fixed 1:n mapping problem
    Sometimes, 1 ENSEMBL ID could map to multiple cross-references.

--- src/main/java/org/reactome/addlinks/fileprocessors/ensembl/EnsemblFileAggregator.java
+++ src/main/java/org/reactome/addlinks/fileprocessors/ensembl/EnsemblFileAggregator.java
@@ -8,6 +8,7 @@ import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.nio.file.attribute.FileAttribute;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -40,36 +41,64 @@ public class EnsemblFileAggregator
 		this.dbNames = dbNames;
 		this.rootPath = rootPath;
 	}
+
+	private String[] parseLine(String fileNamePattern, String line) throws Error
+	{
+		String parts[];
+		if (fileNamePattern.toLowerCase().endsWith("csv"))
+		{
+			parts = line.split(",");
+		}
+		else if (fileNamePattern.toLowerCase().endsWith("tsv"))
+		{
+			parts = line.split("\t");
+		}
+		else
+		{
+			logger.error("File {} does not end with \"csv\" or \"tsv\", so it cannot be determined how to process it.", fileNamePattern);
+			throw new Error ("File \""+fileNamePattern+"\" must be either a tsv or csv.");
+		}
+		return parts;
+	}
+
 	
-	private void buildMappingFromFile(String fileNamePattern, Map<String, String> mapping, boolean reverse)
+	private void buildMappingListsFromFile(String fileNamePattern, Map<String, List<String>> mapping)
 	{
 		try
 		{
 			Files.lines(Paths.get(fileNamePattern)).sequential().forEach( line -> {
-				String parts[];
-				if (fileNamePattern.toLowerCase().endsWith("csv"))
+				String[] parts = parseLine(fileNamePattern, line);
+				//mapping.put(parts[0], parts[1]);
+				if (mapping.containsKey(parts[0]))
 				{
-					parts = line.split(",");
-				}
-				else if (fileNamePattern.toLowerCase().endsWith("tsv"))
-				{
-					parts = line.split("\t");
+					// no point in having duplicates
+					if (!mapping.get(parts[0]).contains(parts[1]))
+					{
+						mapping.get(parts[0]).add(parts[1]);
+						logger.trace("{} maps to multiple cross-references {} ", parts[0],mapping.get(parts[0]));
+					}
 				}
 				else
 				{
-					logger.error("File {} does not end with \"csv\" or \"tsv\", so it cannot be determined how to process it.", fileNamePattern);
-					throw new Error ("File \""+fileNamePattern+"\" must be either a tsv or csv.");
+					mapping.put(parts[0], new ArrayList<String>(Arrays.asList(parts[1])) );
 				}
+				
+			});
+		}
+		catch (IOException e)
+		{
+			throw new Error(e);
+		}
+	}
 
-				if (reverse)
-				{
-					// this is so that if there are many cross-references to 1 ENSEMBL ID, we can track all of them.
-					mapping.put(parts[1], parts[0]);
-				}
-				else
-				{
-					mapping.put(parts[0], parts[1]);
-				}
+	private void buildMappingFromFile(String fileNamePattern, Map<String, String> mapping)
+	{
+		try
+		{
+			Files.lines(Paths.get(fileNamePattern)).sequential().forEach( line -> {
+				String[] parts = parseLine(fileNamePattern, line);
+				mapping.put(parts[0], parts[1]);
+				
 			});
 		}
 		catch (IOException e)
@@ -84,19 +113,19 @@ public class EnsemblFileAggregator
 		
 		Map<String, String> enspToEnst = new HashMap<String, String>();
 		Map<String, String> enstToEnsg = new HashMap<String, String>();
-		Map<String,Map<String, String>> ensgToXrefs = new HashMap<String, Map<String, String>>();
+		Map<String,Map<String, List<String>>> ensgToXrefs = new HashMap<String, Map<String, List<String>>>();
 		//first, process the ENSP file which will map to ENST.
-		buildMappingFromFile(rootPath + "/ENSP_batch_lookup." + this.speciesID + ".xml.transformed.csv", enspToEnst, false);
+		buildMappingFromFile(rootPath + "/ENSP_batch_lookup." + this.speciesID + ".xml.transformed.csv", enspToEnst);
 		logger.info("{} ENSP->ENST mappings.", enspToEnst.size());
 		//second, process the ENST lookup file with ENST -> ENSG mappings.
-		buildMappingFromFile(rootPath + "/ENST_batch_lookup." + this.speciesID + ".xml.transformed.csv", enstToEnsg, false);
+		buildMappingFromFile(rootPath + "/ENST_batch_lookup." + this.speciesID + ".xml.transformed.csv", enstToEnsg);
 		logger.info("{} ENST->ENSG mappings.", enstToEnsg.size());
 		//third, process the file which maps ENSG to cross-references.
 		for (String dbName : this.dbNames)
 		{
-			ensgToXrefs.put(dbName, new HashMap<String, String>());
+			ensgToXrefs.put(dbName, new HashMap<String, List<String>>());
 			
-			buildMappingFromFile(rootPath + "/ensembl_to_ALL." + this.speciesID + ".xml." + dbName + ".transformed.tsv", ensgToXrefs.get(dbName), true);
+			buildMappingListsFromFile(rootPath + "/ensembl_to_ALL." + this.speciesID + ".xml." + dbName + ".transformed.tsv", ensgToXrefs.get(dbName));
 			logger.info("for {}, {} ENSG->Cross-Reference mappings.", dbName, ensgToXrefs.get(dbName).size());
 		}
 		
@@ -110,10 +139,20 @@ public class EnsemblFileAggregator
 				EnsemblMapping ensemblMapping = null;
 
 				//String xref = ensgToXref.get(ensg);
-				String xref = ensgToXrefs.get(dbName).get(ensg);
-			
-				ensemblMapping = new EnsemblMapping(ensp, enst, ensg, xref, dbName);
-				mappings.add(ensemblMapping);
+				//String xref = ensgToXrefs.get(dbName).get(ensg);
+				if (ensgToXrefs.get(dbName).get(ensg) != null)
+				{
+					for (String xref : ensgToXrefs.get(dbName).get(ensg) )
+					{
+						ensemblMapping = new EnsemblMapping(ensp, enst, ensg, xref, dbName);
+						mappings.add(ensemblMapping);
+					}
+				}
+				else
+				{
+					ensemblMapping = new EnsemblMapping(ensp, enst, ensg, null, dbName);
+					mappings.add(ensemblMapping);
+				}
 			}
 		}
 		
