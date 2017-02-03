28110f25ff5cdc4989e54cb6917f409456f67fd4
SolomonShorser-OICR <solomon.shorser@oicr.on.ca>
2017-02-03 16:18:58 -0500

    IntEnz reference creation.

--- src/test/java/org/reactome/addlinks/test/TestIntEnzIT.java
+++ src/test/java/org/reactome/addlinks/test/TestIntEnzIT.java
@@ -5,10 +5,13 @@ import static org.junit.Assert.*;
 import java.util.List;
 import java.util.Map;
 
+import org.gk.model.GKInstance;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.reactome.addlinks.dataretrieval.FileRetriever;
+import org.reactome.addlinks.db.ReferenceObjectCache;
 import org.reactome.addlinks.fileprocessors.IntEnzFileProcessor;
+import org.reactome.addlinks.referencecreators.IntEnzReferenceCreator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 
@@ -22,6 +25,12 @@ public class TestIntEnzIT
 	
 	@Autowired
 	IntEnzFileProcessor intEnzFileProcessor;
+	
+	@Autowired
+	IntEnzReferenceCreator intEnzRefCreator;
+	
+	@Autowired
+	ReferenceObjectCache objectCache;
 	
 	@Test
 	public void testGetIntEnzFile() throws Exception
@@ -39,5 +48,20 @@ public class TestIntEnzIT
 		
 		assertNotNull(mappings);
 		assertTrue(mappings.keySet().size() > 0);
+	}
+	
+	@Test
+	public void testCreateIntEnzReferences() throws Exception
+	{
+		intEnzFileRetriever.fetchData();
+		
+		Map<String, List<String>> mappings = intEnzFileProcessor.getIdMappingsFromFile();
+		
+		assertNotNull(mappings);
+		assertTrue(mappings.keySet().size() > 0);
+		
+		List<GKInstance> sourceReferences = objectCache.getByRefDb(objectCache.getRefDbNamesToIds().get("UniProt").get(0), "ReferenceGeneProduct");
+		
+		intEnzRefCreator.createIdentifiers(123456789, mappings, sourceReferences);
 	}
 }
