2a90d434a6baa0994088ee01f0e90a3368047b72
SolomonShorser-OICR <solomon.shorser@oicr.on.ca>
2017-02-21 14:17:15 -0500

    Create Rhea references.

---
+++ src/main/java/org/reactome/addlinks/referencecreators/RHEAReferenceCreator.java
@@ -0,0 +1,95 @@
+package org.reactome.addlinks.referencecreators;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.gk.model.GKInstance;
+import org.gk.model.ReactomeJavaConstants;
+import org.gk.persistence.MySQLAdaptor;
+import org.gk.schema.InvalidAttributeException;
+
+public class RHEAReferenceCreator extends SimpleReferenceCreator<List <String>>
+{
+	private static final Logger logger = LogManager.getLogger();
+	
+	public RHEAReferenceCreator(MySQLAdaptor adapter, String classToCreate, String classReferring, String referringAttribute, String sourceDB, String targetDB)
+	{
+		super(adapter, classToCreate, classReferring, referringAttribute, sourceDB, targetDB);
+	}
+
+	/**
+	 * "mapping" is a map of Rhea IDs to lists of Reactome Stable IDs (for reactions).
+	 */
+	@Override
+	public void createIdentifiers(long personID, Map<String, List<String>> mapping, List<GKInstance> sourceReferences) throws Exception
+	{
+		int sourceIdentifiersWithNoMapping = 0;
+		int sourceIdentifiersWithNewIdentifier = 0;
+		int sourceIdentifiersWithExistingIdentifier = 0;
+		
+		for (String rheaID : mapping.keySet())
+		{
+			for (String reactomeID : mapping.get(rheaID))
+			{
+				// We have to check each of these reactions to make sure it is: 1) in sourceReferences 2) doesn't already have a xref to rheaID
+				List<GKInstance> instances = getIdentifiersInList(sourceReferences, reactomeID);
+				// Really, I don't expect more than 1 instance, but it *could* return multiple instances, so...
+				if (instances.size() > 0)
+				{
+					for (GKInstance reaction : instances)
+					{
+						if (!this.checkXRefExists(reaction, rheaID))
+						{
+							sourceIdentifiersWithNewIdentifier++;
+							if (!this.testMode)
+							{
+								this.refCreator.createIdentifier(rheaID, reactomeID,  this.targetRefDB, personID, this.getClass().getName());
+							}
+						}
+						else
+						{
+							sourceIdentifiersWithExistingIdentifier++;
+						}
+					}
+				}
+				else
+				{
+					sourceIdentifiersWithNoMapping++;
+				}
+			}
+		}
+		logger.info("{} reference creation summary: \n"
+				+ "\t# {} IDs with a new {} identifier (a new {} reference was created): {};\n"
+				+ "\t# {} identifiers which already had the same {} reference (nothing new was created): {};\n"
+				+ "\t# {} identifiers not in the {} mapping file (no new {} reference was created for them): {} ",
+				this.targetRefDB,
+				this.sourceRefDB, this.targetRefDB, this.targetRefDB, sourceIdentifiersWithNewIdentifier,
+				this.sourceRefDB, this.targetRefDB, sourceIdentifiersWithExistingIdentifier,
+				this.sourceRefDB, this.targetRefDB, this.targetRefDB, sourceIdentifiersWithNoMapping);
+	}
+	
+	private List<GKInstance> getIdentifiersInList(List<GKInstance> instances, String reactomeID)
+	{
+		List<GKInstance> matchingInstances = instances.parallelStream().filter( instance -> {
+			try
+			{
+				return ((GKInstance)instance.getAttributeValue(ReactomeJavaConstants.stableIdentifier)).getAttributeValue(ReactomeJavaConstants._displayName).toString().equals(reactomeID);
+			}
+			catch (InvalidAttributeException e)
+			{
+				e.printStackTrace();
+				return false;
+			}
+			catch (Exception e)
+			{
+				e.printStackTrace();
+				return false;
+			}
+		} ).map(instance -> instance).collect(Collectors.toList());
+		
+		return matchingInstances;
+	}
+}
--- src/test/java/org/reactome/addlinks/test/TestRHEA.java
+++ src/test/java/org/reactome/addlinks/test/TestRHEA.java
@@ -1,19 +1,20 @@
 package org.reactome.addlinks.test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 import org.gk.model.GKInstance;
-import org.gk.model.ReactomeJavaConstants;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.reactome.addlinks.dataretrieval.FileRetriever;
-import org.reactome.addlinks.dataretrieval.RHEAFileRetriever;
 import org.reactome.addlinks.db.ReferenceObjectCache;
 import org.reactome.addlinks.fileprocessors.RHEAFileProcessor;
+import org.reactome.addlinks.referencecreators.RHEAReferenceCreator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 
@@ -29,6 +30,9 @@ public class TestRHEA
 	
 	@Autowired
 	RHEAFileProcessor rheaFileProcessor;
+	
+	@Autowired
+	RHEAReferenceCreator rheaReferenceCreator;
 	
 	@Test
 	public void testGetRheaFiles()
@@ -55,6 +59,28 @@ public class TestRHEA
 			assertNotNull(mappings);
 			assertTrue(mappings.keySet().size() > 0);
 			
+		}
+		catch (Exception e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			fail();
+		}
+		
+	}
+	
+	@Test
+	public void testCreateRheaReferences()
+	{
+		try
+		{
+			rheaRetriever.fetchData();
+			Map<String, List<String> > mappings = rheaFileProcessor.getIdMappingsFromFile();
+			assertNotNull(mappings);
+			assertTrue(mappings.keySet().size() > 0);
+		
+			List<GKInstance> sourceReferences = objectCache.getReactionsByID().values().stream().collect(Collectors.toList());
+			rheaReferenceCreator.createIdentifiers(123465789L, mappings, sourceReferences);
 		}
 		catch (Exception e)
 		{
