ca42159a4c24d04c8d08cb38523e49293a47fdda
SolomonShorser-OICR <solomon.shorser@oicr.on.ca>
2017-03-17 11:06:28 -0400

    It will be easier to track things it it logs to retrievers/Uniprot.log

--- src/main/java/org/reactome/addlinks/uniprot/UniProtFileRetreiverExecutor.java
+++ src/main/java/org/reactome/addlinks/uniprot/UniProtFileRetreiverExecutor.java
@@ -25,7 +25,7 @@ import org.reactome.addlinks.db.ReferenceObjectCache;
 
 public class UniProtFileRetreiverExecutor implements CustomLoggable
 {
-	private Logger logger;
+	private Logger logger;// = LogManager.getLogger();
 	private Map<String, UniprotFileRetreiver> uniprotFileRetrievers;
 	private List<String> fileRetrieverFilter;
 	private ReferenceObjectCache objectCache;
@@ -33,7 +33,7 @@ public class UniProtFileRetreiverExecutor implements CustomLoggable
 	
 	public UniProtFileRetreiverExecutor()
 	{
-		this.logger = this.createLogger("retrievers/UniProtFileRetreiverExecutor", "RollingRandomAccessFile", "UniProtFileRetreiverExecutor", true, Level.DEBUG, this.logger, "UniProtFileRetreiverExecutor");
+		this.logger = this.createLogger("retrievers/Uniprot", "RollingRandomAccessFile", this.getClass().getName(), true, Level.DEBUG, this.logger, "UniProtFileRetreiverExecutor");
 	}
 	
 	public void execute()
