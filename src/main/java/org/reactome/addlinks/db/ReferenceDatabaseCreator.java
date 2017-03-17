7a0a620848bacc7c77f66633debb2b21ace0be5d
SolomonShorser-OICR <solomon.shorser@oicr.on.ca>
2017-03-17 16:55:38 -0400

    Create reference databases with Primary name and some aliases.

--- src/main/java/org/reactome/addlinks/db/ReferenceDatabaseCreator.java
+++ src/main/java/org/reactome/addlinks/db/ReferenceDatabaseCreator.java
@@ -12,6 +12,8 @@ import org.gk.model.GKInstance;
 import org.gk.model.InstanceDisplayNameGenerator;
 import org.gk.model.ReactomeJavaConstants;
 import org.gk.persistence.MySQLAdaptor;
+import org.gk.schema.InvalidAttributeException;
+import org.gk.schema.InvalidAttributeValueException;
 import org.gk.schema.SchemaAttribute;
 import org.gk.schema.SchemaClass;
 
@@ -26,15 +28,88 @@ public class ReferenceDatabaseCreator
 	}
 	
 	/**
+	 * Creates a reference database with a primary name and some (optional) aliases.
+	 * This will not create a reference database if an existing reference database has the same primaryName. 
+	 * @param url - The URL of the ReferenceDatabase.
+	 * @param accessUrl - The access URL of the RefereneDatabase.
+	 * @param primaryName - The primary name for this reference database (will have name_rank==0)
+	 * @param aliases - Other names.
+	 * @throws Exception 
+	 */
+	public void createReferenceDatabaseWithAliases(String url, String accessUrl, String primaryName, String ... aliases) throws Exception
+	{
+		//First, let's check that the Reference Database doesn't already exist. all we have to go on is the name...
+		try
+		{
+			SchemaClass refDBClass = adapter.getSchema().getClassByName(ReactomeJavaConstants.ReferenceDatabase);
+			SchemaAttribute dbNameAttrib = refDBClass.getAttribute(ReactomeJavaConstants.name);
+			@SuppressWarnings("unchecked")
+			Collection<GKInstance> preexistingReferenceDBs = (Collection<GKInstance>) adapter.fetchInstanceByAttribute(dbNameAttrib, "=", primaryName);
+			// Now that we have a bunch of things that contain primaryName, we need to find the ones where the rank of that name-attribute is 0.
+			if (preexistingReferenceDBs!=null && preexistingReferenceDBs.size() > 0)
+			{
+				for (GKInstance refDBInst : preexistingReferenceDBs)
+				{
+					// It looks like the MySQLAdapter class will order the results by "*_rank" so the primary name
+					// should be the first thing in the list, if there's anything at all.
+					@SuppressWarnings("unchecked")
+					List<String> names = (List<String>) refDBInst.getAttributeValuesList(ReactomeJavaConstants.name);
+					
+					// if primaryName is not already in use...
+					if (names==null || names.size() == 0
+						|| !(names.get(0).equals(primaryName)) )
+					{
+						createRefDBWithAliases(url, accessUrl, primaryName, refDBClass, aliases);
+					}
+					else
+					{
+						logger.warn("The primaryName {} appears to already be in use by {}", primaryName, refDBInst);
+					}
+				}
+			}
+			else
+			{
+				createRefDBWithAliases(url, accessUrl, primaryName, refDBClass, aliases);
+			}
+			
+		}
+		catch (Exception e)
+		{
+			logger.error("Error while trying to create a Reference Database object: "+e.getMessage());
+			e.printStackTrace();
+			throw e;
+		}
+	}
+
+	private void createRefDBWithAliases(String url, String accessUrl, String primaryName, SchemaClass refDBClass, String... aliases) throws InvalidAttributeException, InvalidAttributeValueException, Exception
+	{
+		// Create new GKInstance, with the appropriate primary name.
+		GKInstance newReferenceDB = new GKInstance(refDBClass);
+		newReferenceDB.addAttributeValue(ReactomeJavaConstants.name, primaryName);
+		// Add aliases.
+		for (String alias : aliases)
+		{
+			newReferenceDB.addAttributeValue(ReactomeJavaConstants.name, alias);
+		}
+		// Set othe attributes.
+		newReferenceDB.setAttributeValue(ReactomeJavaConstants.url, url);
+		newReferenceDB.setAttributeValue(ReactomeJavaConstants.accessUrl, accessUrl);
+		newReferenceDB.setDbAdaptor(this.adapter);
+		InstanceDisplayNameGenerator.setDisplayName(newReferenceDB);
+		//Persist to storage.
+		this.adapter.storeInstance(newReferenceDB);
+	}
+	
+	/**
 	 * Creates a ReferenceDatabase. If the ReferenceDatabase already exists, it will not be created.
 	 * @param url - The URL of the ReferenceDatabase.
 	 * @param accessUrl - The access URL of the RefereneDatabase.
-	 * @param names - A list of names for this ReferenceDatabase. If none of these values are already in the database, then a new ReferenceDatabase
-	 * will be created and these names will be associated with it. If any of these names already exist, and the other names will be associated with it.
-	 * The URLs will not be updated in the case that a ReferenceDatabase name is pre-existing in the database.
+	 * @param names - A list of names for this ReferenceDatabase. If *none* of these values are already in the database, then a new ReferenceDatabase
+	 * will be created and these names will be associated with it. If *any* of these names already exist, then the other names will be associated with it.
+	 * The URLs will *not* be updated in the case that a ReferenceDatabase name is pre-existing in the database.
 	 * @throws Exception 
 	 */
-	public void createReferenceDatabase(String url, String accessUrl, String ... names) throws Exception
+	public void createReferenceDatabaseToURL(String url, String accessUrl, String ... names) throws Exception
 	{
 		//First, let's check that the Reference Database doesn't already exist. all we have to go on is the name...
 		SchemaClass refDBClass = adapter.getSchema().getClassByName(ReactomeJavaConstants.ReferenceDatabase);
@@ -103,7 +178,7 @@ public class ReferenceDatabaseCreator
 		}
 		catch (Exception e)
 		{
-			logger.error("Error while trying to create a Reference Dataabse object: "+e.getMessage());
+			logger.error("Error while trying to create a Reference Database object: "+e.getMessage());
 			throw e;
 		}
 	}
