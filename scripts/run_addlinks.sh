406ee691d725fcb7216f5c8a24a049e2d349e3f7
SolomonShorser-OICR <solomon.shorser@oicr.on.ca>
2017-04-04 15:15:32 -0400

    Add comment.

--- scripts/run_addlinks.sh
+++ scripts/run_addlinks.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
+# Make sure you have the AddLinks jar file (or a link to it) named "AddLinks.jar" in this directory, as well as the resources directory (or a link to it).
+
 java -cp "$(pwd)/resources" \
 	-Dlog4j.configurationFile=$(pwd)/resources/log4j2.xml \
 	-Dconfig.location=$(pwd)/resources/addlinks.properties \
