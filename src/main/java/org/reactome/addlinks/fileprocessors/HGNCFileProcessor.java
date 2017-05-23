71081e7331a670ace85e4c8f3b44dc9780753565
SolomonShorser-OICR <solomon.shorser@oicr.on.ca>
2017-05-23 15:57:22 -0400

    HGNC reference creator.

--- src/main/java/org/reactome/addlinks/fileprocessors/HGNCFileProcessor.java
+++ src/main/java/org/reactome/addlinks/fileprocessors/HGNCFileProcessor.java
@@ -2,6 +2,7 @@ package org.reactome.addlinks.fileprocessors;
 
 import java.io.IOException;
 import java.nio.file.Files;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
@@ -24,11 +25,10 @@ public class HGNCFileProcessor extends FileProcessor<List<String>>
 	@Override
 	public  Map<String, List<String>> getIdMappingsFromFile()
 	{
-		// HGNC symbol is in the first column. Uniprot ID is in the 26th column.  Will will map 1:n, HGNC to Uniprot
+		// HGNC symbol is in the first column. Uniprot ID is in the 26th column.  Will will map 1:n, UniProt to HGNC
 		// A HGNC Id could map to multiple Uniprot IDs, they will be separated by "|".
 		Map<String, List<String>> mappings = new HashMap<String, List<String>>();
 		int lineCount = 0;
-		int hgncWithUniProt = 0;
 		try
 		{
 			//Read the file, skipping the first line (which is just column headers).
@@ -40,18 +40,23 @@ public class HGNCFileProcessor extends FileProcessor<List<String>>
 				String hgncID = parts[0].replaceAll("HGNC:", "");
 				if (parts.length >= 26)
 				{
-					String uniprotID = parts[25];
-					if (uniprotID != null)
+					String uniprotIDLine = parts[25];
+					if (uniprotIDLine != null)
 					{
-						hgncWithUniProt++;
-	//					if (uniprotID.contains("|"))
-	//					{
-							String[] uniprotIDs = uniprotID.split("\\|");
-	//					}
-						
+						String[] uniprotIDs = uniprotIDLine.split("\\|");
 						if (uniprotIDs.length > 0)
 						{
-							mappings.put(hgncID, Arrays.asList(uniprotIDs));
+							for (String uniprotID : uniprotIDs)
+							{
+								if (!mappings.containsKey(uniprotID))
+								{
+									mappings.put(uniprotID, new ArrayList<String>(Arrays.asList(hgncID)));
+								}
+								else
+								{
+									mappings.get(uniprotID).add(hgncID);
+								}
+							}
 						}
 					}
 				}
