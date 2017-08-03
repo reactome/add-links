5d8ee7b1e5c950c6895862331d3492d52e39cf14
SolomonShorser-OICR <solomon.shorser@oicr.on.ca>
2017-08-03 15:57:30 -0400

    Actually, I think "-Dlog4j.configurationFile" is no longer needed.

--- scripts/run_addlinks.sh
+++ scripts/run_addlinks.sh
@@ -3,6 +3,5 @@
 # Make sure you have the AddLinks jar file (or a link to it) named "AddLinks.jar" in this directory, as well as the resources directory (or a link to it).
 
 java -cp "$(pwd)/resources" \
-	-Dlog4j.configurationFile=$(pwd)/resources/log4j2.xml \
 	-Dconfig.location=$(pwd)/resources/addlinks.properties \
 	-jar AddLinks.jar file://$(pwd)/resources/application-context.xml
