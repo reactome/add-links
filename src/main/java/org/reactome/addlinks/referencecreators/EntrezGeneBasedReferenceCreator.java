e89cfb3a34cd33aacad04f3ccbd878cd029ec1b2
SolomonShorser-OICR <solomon.shorser@oicr.on.ca>
2017-08-22 15:07:06 -0400

    Check for pre-existing cross-references to avoid duplicates.

--- src/main/java/org/reactome/addlinks/referencecreators/EntrezGeneBasedReferenceCreator.java
+++ src/main/java/org/reactome/addlinks/referencecreators/EntrezGeneBasedReferenceCreator.java
@@ -1,7 +1,11 @@
 package org.reactome.addlinks.referencecreators;
 
+import java.util.List;
+
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.Logger;
+import org.gk.model.GKInstance;
+import org.gk.model.ReactomeJavaConstants;
 import org.gk.persistence.MySQLAdaptor;
 
 /**
@@ -22,6 +26,41 @@ public class EntrezGeneBasedReferenceCreator extends SimpleReferenceCreator<Stri
 	
 	public void createEntrezGeneReference(String identifierValue, String referencedObject, String speciesID, Long personID) throws Exception
 	{
-		this.refCreator.createIdentifier(identifierValue, referencedObject, this.targetRefDB, personID, this.getClass().getName());
+		logger.trace("Possible new identifier {} for {}", identifierValue, referencedObject);
+		GKInstance inst = this.adapter.fetchInstance(new Long(referencedObject));
+		@SuppressWarnings("unchecked")
+		List<GKInstance> xrefs = (List<GKInstance>) inst.getAttributeValuesList(referringAttributeName);
+		boolean xrefAlreadyExists = false;
+		String xrefString = "";
+		for (GKInstance xref : xrefs)
+		{
+			String xrefIdentifier = xref.getAttributeValue(ReactomeJavaConstants.identifier) != null
+									? xref.getAttributeValue(ReactomeJavaConstants.identifier).toString()
+									: null;
+			String xrefRefDB = xref.getAttributeValue(ReactomeJavaConstants.referenceDatabase).toString();
+			xrefString += "cross-reference: "+xrefIdentifier+"@"+xrefRefDB+" ; ";
+			
+			// We won't add a cross-reference if it already exists, and for the same Ref database (it's possible that the identifier could be
+			// used in other reference databases).
+			if (identifierValue.equals(xrefIdentifier) && xrefRefDB.contains(this.targetRefDB))
+			{
+				xrefAlreadyExists = true;
+				// Break out of the xrefs loop - we found an existing cross-reference that matches so there's no point 
+				// in letting the loop run longer.
+				// TODO: rewrite into a while-loop condition (I don't like breaks that much).
+				break;
+			}
+		}
+		if (!xrefString.equals(""))
+		{
+			logger.trace("\t {}",xrefString);
+		}
+		if (!xrefAlreadyExists)
+		{
+			logger.trace("\tCreate identifier {} for {} @ ", identifierValue, referencedObject, this.targetRefDB);
+			this.refCreator.createIdentifier(identifierValue, referencedObject, this.targetRefDB, personID, this.getClass().getName());
+		}
+		
+		
 	}
 }
