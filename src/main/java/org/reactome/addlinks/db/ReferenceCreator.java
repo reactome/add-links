ebd07e643231047b9496e07d54c618bd0e8a7d20
SolomonShorser-OICR <solomon.shorser@oicr.on.ca>
2017-09-28 13:37:55 -0400

    FIXED: The issued with empty KEGG identifiers!

--- src/main/java/org/reactome/addlinks/db/ReferenceCreator.java
+++ src/main/java/org/reactome/addlinks/db/ReferenceCreator.java
@@ -131,10 +131,10 @@ public class ReferenceCreator
 			// reference creator. Since UniProt references are downloaded, processed, and created in a fairly generic fashion,
 			// there is no easy way to integrate this sort of check in the UniProt-related classes.
 			boolean needToRemoveSpeciesCode = this.refDBInstance.getAttributeValue(ReactomeJavaConstants.name).toString().startsWith("KEGG")
-											&& identifierValue.length() > 3 && this.refDBInstance.getAttributeValue(ReactomeJavaConstants.accessUrl).toString().contains(identifierValue.substring(0, 3));
+											&& identifierValue.length() > 3 && this.refDBInstance.getAttributeValue(ReactomeJavaConstants.accessUrl).toString().contains( (identifierValue.substring(0, 3)+":") );
 			if ( needToRemoveSpeciesCode )
 			{
-				identifierValue = identifierValue.substring(4);
+				identifierValue = identifierValue.replaceFirst(identifierValue.substring(3)+":", "");
 			}
 			
 			GKSchemaAttribute identifierAttribute = (GKSchemaAttribute) this.schemaClass.getAttribute(ReactomeJavaConstants.identifier);
