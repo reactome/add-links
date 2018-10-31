b6f3fd3bb1161bfb4e7ae22bae260a1ec1050df6
SolomonShorser-OICR <solomon.shorser@oicr.on.ca>
2018-10-31 12:50:43 -0400

    Update query to include species info in the results.

--- src/main/java/org/reactome/addlinks/db/DuplicateIdentifierReporter.java
+++ src/main/java/org/reactome/addlinks/db/DuplicateIdentifierReporter.java
@@ -11,7 +11,7 @@ public class DuplicateIdentifierReporter
 {
 	private MySQLAdaptor dbAdapter;
 	
-	private static String query = "select subq1.*, ReferenceDatabase_2_name.name\n" + 
+	private static String query = "SELECT distinct subq1.*, ReferenceDatabase_2_name.name, Species.db_id, Taxon_2_name.name\n" + 
 			"from (\n" + 
 			"	select count(ReferenceEntity.DB_ID) as duplicate_count, ReferenceEntity.identifier, group_concat(ReferenceEntity.db_id) as ReferenceEntity_DB_IDs, ReferenceEntity.referenceDatabase, DatabaseObject._class, subq.combined_identifier\n" + 
 			"	from ReferenceEntity \n" + 
@@ -24,10 +24,17 @@ public class DuplicateIdentifierReporter
 			"	-- First, group on the combined identifer - the identifier concatenated with _displayName.\n" + 
 			"    -- This is because an identifier could be associated with many DatabaseObjects that have different _displayNames, sometimes the\n" + 
 			"    -- _displayName indiciates as version-number change in an object, but the reference identifier remains the same.\n" + 
-			"    group by subq.combined_identifier, ReferenceEntity.identifier, referenceDatabase, _class\n" + 
+			"	group by subq.combined_identifier, ReferenceEntity.identifier, referenceDatabase, _class\n" + 
 			"	having count(ReferenceEntity.db_id) > 1) as subq1\n" + 
 			"inner join ReferenceDatabase_2_name on ReferenceDatabase_2_name.DB_ID = subq1.referenceDatabase\n" + 
+			"INNER JOIN ReferenceEntity ON ReferenceEntity.identifier = subq1.identifier\n" +
+			"-- start OUTER joining from ReferenceEntity through ReferenceSequence to Taxon2Name to get the species name of\n" +
+			"-- the reference object. But not all References have a species (such as ReferenceMolecule),\n" +
+			"-- hence the OUTER joins.\n" +
+			"LEFT OUTER JOIN ReferenceSequence ON ReferenceEntity.DB_ID = ReferenceSequence.DB_ID\n" + 
+			"LEFT OUTER JOIN Species ON ReferenceSequence.species = Species.DB_ID\n" + 
+			"LEFT OUTER JOIN Taxon_2_name ON (Taxon_2_name.DB_ID = Species.DB_ID AND Taxon_2_name.name_rank = 0)\n" + 
 			"where ReferenceDatabase_2_name.name_rank = 0\n" + 
-			"order by duplicate_count desc, identifier asc;";
+			"order by duplicate_count desc, ReferenceDatabase_2_name.name asc;";
 	
 }
