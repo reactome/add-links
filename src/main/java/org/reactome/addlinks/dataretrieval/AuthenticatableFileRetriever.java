119f1ae77b555a97d7be9a15cb64e81ca379020f
SolomonShorser-OICR <solomon.shorser@oicr.on.ca>
2019-02-25 17:34:28 -0500

    Basic documentation.

--- src/main/java/org/reactome/addlinks/dataretrieval/AuthenticatableFileRetriever.java
+++ src/main/java/org/reactome/addlinks/dataretrieval/AuthenticatableFileRetriever.java
@@ -1,5 +1,11 @@
 package org.reactome.addlinks.dataretrieval;
 
+/**
+ * This class is to be extended by File Retrievers that need to be able to
+ * authenticate themselves with the peer from which they retrieve data.
+ * @author sshorser
+ *
+ */
 abstract class AuthenticatableFileRetriever extends FileRetriever
 {
 	public AuthenticatableFileRetriever(String retrieverName)
@@ -15,10 +21,19 @@ abstract class AuthenticatableFileRetriever extends FileRetriever
 	protected String userName;
 	protected String password;
 	
+	/**
+	 * Set the user name.
+	 * @param userName
+	 */
 	public void setUserName(String userName)
 	{
 		this.userName = userName;
 	}
+	
+	/**
+	 * Set the password.
+	 * @param password
+	 */
 	public void setPassword(String password)
 	{
 		this.password = password;
