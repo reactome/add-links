b49ee2b108117de9d0adac4aa3a2c461344da5bc
SolomonShorser-OICR <solomon.shorser@oicr.on.ca>
2019-03-08 13:34:55 -0500

    Test Uniprot Retriever retrying. Also some minor clean-up.

--- src/test/java/org/reactome/addlinks/test/TestUniprotFileRetriever.java
+++ src/test/java/org/reactome/addlinks/test/TestUniprotFileRetriever.java
@@ -1,24 +1,36 @@
 package org.reactome.addlinks.test;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 import java.io.BufferedInputStream;
+import java.io.ByteArrayInputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.time.Duration;
 import java.util.List;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.StatusLine;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PowerMockIgnore;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 import org.reactome.addlinks.dataretrieval.UniprotFileRetreiver;
 
 
-@PowerMockIgnore({"javax.management.*","javax.net.ssl.*"})
+@PowerMockIgnore({"javax.management.*","javax.net.ssl.*", "javax.security.*"})
 @RunWith(PowerMockRunner.class)
 @PrepareForTest({ java.net.URI.class,
 				org.apache.commons.net.ftp.FTPClient.class,
@@ -28,7 +40,7 @@ public class TestUniprotFileRetriever
 {
 
 	@Test
-	public void testUniProtFileRetriever() throws URISyntaxException, IOException
+	public void testUniProtFileRetrieverIT() throws URISyntaxException, IOException
 	{	
 		//Some logging/debugging for monitoring the connection to the server.
 		System.setProperty("org.apache.commons.logging.Log","org.apache.commons.logging.impl.SimpleLog");
@@ -41,7 +53,7 @@ public class TestUniprotFileRetriever
 		System.setProperty("org.apache.commons.logging.simplelog.log.org.apache.http", "DEBUG");
 		
 		UniprotFileRetreiver retriever = new UniprotFileRetreiver();
-		retriever.setDataURL(new URI("http://www.uniprot.org/uploadlists/"));
+		retriever.setDataURL(new URI("https://www.uniprot.org/uploadlists/"));
 		FileInputStream inStream = new FileInputStream(this.getClass().getClassLoader().getResource("uniprot_ids.txt").getFile());
 		BufferedInputStream bis = new BufferedInputStream(inStream);
 		retriever.setDataInputStream(bis);
@@ -52,6 +64,67 @@ public class TestUniprotFileRetriever
 		retriever.setMaxAge(Duration.ofSeconds(1));
 		retriever.setMapFromDb(mapFrom);
 		retriever.setMapToDb(mapTo);
+		retriever.downloadData();
+		
+		System.out.println("Contents of "+pathToFile);
+		List<String> lines = Files.readAllLines(Paths.get(pathToFile));
+		for (String line : lines)
+		{
+			System.out.println(line);
+		}
+		
+		System.out.println("Contents of "+pathToFile.replace(".txt", ".notMapped.txt"));
+		lines = Files.readAllLines(Paths.get(pathToFile.replace(".txt", ".notMapped.txt")));
+		for (String line : lines)
+		{
+			System.out.println(line);
+		}
+	}
+	
+	@Test
+	public void testUniProtFileRetrieverRetryingIT() throws URISyntaxException, IOException
+	{	
+		//Some logging/debugging for monitoring the connection to the server.
+		System.setProperty("org.apache.commons.logging.Log","org.apache.commons.logging.impl.SimpleLog");
+		System.setProperty("org.apache.commons.logging.simplelog.showdatetime", "true");
+		System.setProperty("org.apache.commons.logging.simplelog.log.org.apache.http.wire", "DEBUG");
+		
+		System.setProperty("org.apache.commons.logging.simplelog.log.org.apache.http.impl.conn", "DEBUG");
+		System.setProperty("org.apache.commons.logging.simplelog.log.org.apache.http.impl.client", "DEBUG");
+		System.setProperty("org.apache.commons.logging.simplelog.log.org.apache.http.client", "DEBUG");
+		System.setProperty("org.apache.commons.logging.simplelog.log.org.apache.http", "DEBUG");
+		
+		UniprotFileRetreiver retriever = new UniprotFileRetreiver();
+		retriever.setDataURL(new URI("https://www.uniprot.org/uploadlists/"));
+		FileInputStream inStream = new FileInputStream(this.getClass().getClassLoader().getResource("uniprot_ids.txt").getFile());
+		BufferedInputStream bis = new BufferedInputStream(inStream);
+		retriever.setDataInputStream(bis);
+		String mapFrom=UniprotFileRetreiver.UniprotDB.GeneName.getUniprotName();//"ACC+ID";
+		String mapTo=UniprotFileRetreiver.UniprotDB.KEGG.getUniprotName();//"KEGG_ID";
+		String pathToFile = "/tmp/uniprot_mapping_service/uniprot_xrefs_"+mapFrom+"_to_"+mapTo+".txt";
+		retriever.setFetchDestination(pathToFile);
+		retriever.setMaxAge(Duration.ofSeconds(1));
+		retriever.setMapFromDb(mapFrom);
+		retriever.setMapToDb(mapTo);
+		
+		// Mock the HttpEntity to return NULL the first time, to test retrying.
+		// The second time, it will succeed.
+		// When it comes to get the "not mapped" values, NULL will be returned again,
+		// so we can test running out of retries.
+		CloseableHttpClient httpClient = mock(CloseableHttpClient.class);
+		//HttpGet httpGet = mock(HttpGet.class);
+		CloseableHttpResponse httpResponse = mock(CloseableHttpResponse.class);
+		StatusLine statusLine = mock(StatusLine.class);
+		HttpEntity mockEntity = mock(HttpEntity.class);
+
+		when(statusLine.getStatusCode()).thenReturn(200);
+		when(httpResponse.getStatusLine()).thenReturn(statusLine);
+		when(mockEntity.getContent()).thenReturn(null).thenReturn(new ByteArrayInputStream("blah blah".getBytes(StandardCharsets.UTF_8))).thenReturn(null);
+		when(httpResponse.getEntity()).thenReturn(mockEntity);
+		when(httpClient.execute( any())).thenReturn(httpResponse).thenReturn(httpResponse);
+		PowerMockito.mockStatic(HttpClients.class);
+		when(HttpClients.createDefault()).thenCallRealMethod().thenReturn(httpClient);
+		
 		retriever.downloadData();
 		
 		System.out.println("Contents of "+pathToFile);
