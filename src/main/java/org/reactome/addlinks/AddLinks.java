a7a1bcc4064416a43ea282062d5a8b8eadaa47ea
SolomonShorser-OICR <solomon.shorser@oicr.on.ca>
2019-04-05 17:10:46 -0400

    BIG refactoring, to clean up the AddLinks class.

--- src/main/java/org/reactome/addlinks/AddLinks.java
+++ src/main/java/org/reactome/addlinks/AddLinks.java
@@ -3,16 +3,13 @@ package org.reactome.addlinks;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.sql.SQLException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -21,8 +18,6 @@ import java.util.Set;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.ForkJoinPool;
-import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 import org.apache.http.HttpStatus;
@@ -31,16 +26,17 @@ import org.apache.logging.log4j.Logger;
 import org.gk.model.GKInstance;
 import org.gk.model.ReactomeJavaConstants;
 import org.gk.persistence.MySQLAdaptor;
-import org.gk.schema.InvalidAttributeException;
 import org.reactome.addlinks.brenda.BRENDAReferenceDatabaseGenerator;
-import org.reactome.addlinks.brenda.BRENDASpeciesCache;
 import org.reactome.addlinks.dataretrieval.FileRetriever;
-import org.reactome.addlinks.dataretrieval.KEGGFileRetriever;
 import org.reactome.addlinks.dataretrieval.UniprotFileRetriever;
 import org.reactome.addlinks.dataretrieval.brenda.BRENDAFileRetriever;
 import org.reactome.addlinks.dataretrieval.brenda.BRENDASoapClient;
 import org.reactome.addlinks.dataretrieval.ensembl.EnsemblBatchLookup;
 import org.reactome.addlinks.dataretrieval.ensembl.EnsemblFileRetriever;
+import org.reactome.addlinks.dataretrieval.executor.BrendaFileRetrieverExecutor;
+import org.reactome.addlinks.dataretrieval.executor.KeggFileRetrieverExecutor;
+import org.reactome.addlinks.dataretrieval.executor.SimpleFileRetrieverExecutor;
+import org.reactome.addlinks.dataretrieval.executor.UniprotFileRetrieverExecutor;
 import org.reactome.addlinks.db.CrossReferenceReporter;
 import org.reactome.addlinks.db.DuplicateIdentifierReporter;
 import org.reactome.addlinks.db.DuplicateIdentifierReporter.REPORT_KEYS;
@@ -49,23 +45,19 @@ import org.reactome.addlinks.db.ReferenceObjectCache;
 import org.reactome.addlinks.ensembl.EnsemblFileRetrieverExecutor;
 import org.reactome.addlinks.ensembl.EnsemblReferenceDatabaseGenerator;
 import org.reactome.addlinks.fileprocessors.FileProcessor;
-import org.reactome.addlinks.fileprocessors.ensembl.EnsemblAggregateFileProcessor;
-import org.reactome.addlinks.fileprocessors.ensembl.EnsemblAggregateFileProcessor.EnsemblAggregateProcessingMode;
-import org.reactome.addlinks.fileprocessors.ensembl.EnsemblFileAggregator;
+import org.reactome.addlinks.fileprocessors.ensembl.EnsemblFileProcessorExecutor;
 import org.reactome.addlinks.kegg.KEGGReferenceDatabaseGenerator;
-import org.reactome.addlinks.kegg.KEGGSpeciesCache;
 import org.reactome.addlinks.linkchecking.LinkCheckInfo;
 import org.reactome.addlinks.linkchecking.LinkCheckManager;
 import org.reactome.addlinks.linkchecking.LinksToCheckCache;
 import org.reactome.addlinks.referencecreators.BatchReferenceCreator;
 import org.reactome.addlinks.referencecreators.COSMICReferenceCreator;
-import org.reactome.addlinks.referencecreators.ENSMappedIdentifiersReferenceCreator;
 import org.reactome.addlinks.referencecreators.ComplexPortalReferenceCreator;
+import org.reactome.addlinks.referencecreators.ENSMappedIdentifiersReferenceCreator;
 import org.reactome.addlinks.referencecreators.NCBIGeneBasedReferenceCreator;
 import org.reactome.addlinks.referencecreators.OneToOneReferenceCreator;
 import org.reactome.addlinks.referencecreators.RHEAReferenceCreator;
 import org.reactome.addlinks.referencecreators.UPMappedIdentifiersReferenceCreator;
-import org.reactome.addlinks.uniprot.UniProtFileRetrieverExecutor;
 
 
 public class AddLinks
@@ -155,27 +147,9 @@ public class AddLinks
 		
 		retrieverJobs = new ArrayList<Callable<Boolean>>();
 		// Now that uniprot file retrievers have run, we can run the KEGG file retriever.
-		retrieverJobs.add(new Callable<Boolean>()
-		{
-			
-			@Override
-			public Boolean call() throws Exception
-			{
-				executeKeggFileRetriever();
-				return true;
-			}
-		});
+		retrieverJobs.add(new KeggFileRetrieverExecutor(fileRetrievers, uniprotFileRetrievers, fileRetrieverFilter, objectCache));
 		// Run the Brenda file retriever - it is slow and KEGG is slow, so let's run them together!
-		retrieverJobs.add(new Callable<Boolean>()
-		{
-			
-			@Override
-			public Boolean call() throws Exception
-			{
-				executeBrendaFileRetriever();
-				return true;
-			}
-		});
+		retrieverJobs.add( new BrendaFileRetrieverExecutor(fileRetrievers, fileRetrieverFilter, objectCache));
 		execSrvc.invokeAll(retrieverJobs);
 		
 		logger.info("Finished downloading files.");
@@ -189,7 +163,8 @@ public class AddLinks
 		// Special extra work for ENSEMBL...
 		if (this.fileProcessorFilter.contains("ENSEMBLFileProcessor") || this.fileProcessorFilter.contains("ENSEMBLNonCoreFileProcessor"))
 		{
-			this.processENSEMBLFiles(dbMappings);
+			EnsemblFileProcessorExecutor ensemblFileProcessorExecutor = new EnsemblFileProcessorExecutor(this.dbAdapter, this.objectCache);
+			ensemblFileProcessorExecutor.processENSEMBLFiles(dbMappings);
 		}
 		
 		// Print stats on results of file processing.
@@ -355,44 +330,18 @@ public class AddLinks
 		// We can execute SimpleFileRetrievers at the same time as the UniProt retrievers, and also the ENSEMBL retrievers.
 		
 		List<Callable<Boolean>> retrieverJobs = new ArrayList<>();
-		retrieverJobs.add(new Callable<Boolean>()
-		{
-			@Override
-			public Boolean call() throws Exception
-			{
-				executeSimpleFileRetrievers();
-				return true;
-			}
-		});
+		retrieverJobs.add(new SimpleFileRetrieverExecutor(fileRetrievers, fileRetrieverFilter));
 		// Execute the UniProt file retrievers separately.
-		retrieverJobs.add(new Callable<Boolean>()
-		{
-			
-			@Override
-			public Boolean call() throws Exception
-			{
-				executeUniprotFileRetrievers(numUniprotDownloadThreads);
-				return true;
-			}
-		});
+		retrieverJobs.add(new UniprotFileRetrieverExecutor(uniprotFileRetrievers, fileRetrieverFilter, numUniprotDownloadThreads, objectCache));
 		
 		// Check to see if we should do any Ensembl work
 		if (fileRetrieverFilter.contains("EnsemblToALL"))
 		{
-			retrieverJobs.add(new Callable<Boolean>()
-			{
-				
-				@Override
-				public Boolean call() throws Exception
-				{	
-					executeEnsemblFileRetriever();
-					return true;
-				}
-			});
+			retrieverJobs.add(new EnsemblFileRetrieverExecutor(this.ensemblFileRetrievers, this.ensemblFileRetrieversNonCore , this.fileRetrieverFilter, this.ensemblBatchLookup, this.objectCache, this.dbAdapter));
 		}
 		return retrieverJobs;
 	}
-
+	
 	/**
 	 * Renames all of the BRENDA databases to simply be "BRENDA".
 	 * The names of the BRENDA ReferenceDatabases were getting into a downstream file,
@@ -456,208 +405,6 @@ public class AddLinks
 		
 	}
 
-	/**
-	 * Execut ethe ENSEMBL file retriever. 
-	 * @throws Exception
-	 */
-	private void executeEnsemblFileRetriever() throws Exception
-	{
-		if (fileRetrieverFilter.contains("EnsemblToALL"))
-		{
-			logger.info("Executing ENSEMBL file retrievers");
-			EnsemblFileRetrieverExecutor ensemblFileRetrieverExecutor = new EnsemblFileRetrieverExecutor();
-			ensemblFileRetrieverExecutor.setEnsemblBatchLookup(ensemblBatchLookup);
-			ensemblFileRetrieverExecutor.setEnsemblFileRetrievers(ensemblFileRetrievers);
-			ensemblFileRetrieverExecutor.setEnsemblFileRetrieversNonCore(ensemblFileRetrieversNonCore);
-			ensemblFileRetrieverExecutor.setObjectCache(objectCache);
-			ensemblFileRetrieverExecutor.setDbAdapter(dbAdapter);
-			ensemblFileRetrieverExecutor.execute();
-		}
-	}
-	
-	/**
-	 * Execute the BRENDA file retriever. It's complicated enough to warrant it's own method.
-	 */
-	private void executeBrendaFileRetriever()
-	{
-		BRENDAFileRetriever brendaRetriever = (BRENDAFileRetriever) this.fileRetrievers.get("BrendaRetriever");
-
-		List<String> identifiers = new ArrayList<String>();
-		String originalDestination = brendaRetriever.getFetchDestination();
-
-		if (this.fileRetrieverFilter.contains("BrendaRetriever"))
-		{
-			logger.info("Executing BRENDA file retrievers");
-			
-			for (String speciesName : objectCache.getSetOfSpeciesNames().stream().sorted().collect(Collectors.toList() ) )
-			{
-				String speciesId = objectCache.getSpeciesNamesToIds().get(speciesName).get(0);
-				if (BRENDASpeciesCache.getCache().contains(speciesName.trim()))
-				{
-					List<String> uniprotIdentifiers = objectCache.getByRefDbAndSpecies("2", speciesId, ReactomeJavaConstants.ReferenceGeneProduct).stream().map(instance -> {
-						try
-						{
-							return (String)instance.getAttributeValue(ReactomeJavaConstants.identifier);
-						}
-						catch (InvalidAttributeException e)
-						{
-							e.printStackTrace();
-						}
-						catch (Exception e)
-						{
-							e.printStackTrace();
-						}
-						return null;
-					}).collect(Collectors.toList());
-					
-					logger.info("Processing for Brenda: Species: "+speciesId+"/"+speciesName);
-					identifiers.addAll(uniprotIdentifiers);
-					
-					if (uniprotIdentifiers != null && uniprotIdentifiers.size() > 0)
-					{
-						brendaRetriever.setSpeciesName(speciesName);
-						brendaRetriever.setIdentifiers(uniprotIdentifiers);
-						brendaRetriever.setFetchDestination(originalDestination.replace(".csv","."+speciesName.replace(" ", "_")+".csv"));
-						try
-						{
-							brendaRetriever.fetchData();
-						} catch (Exception e)
-						{
-							logger.error("Error occurred while trying to fetch Brenda data: {}. File may not have been downloaded.", e.getMessage());
-							e.printStackTrace();
-						}
-					}
-					else
-					{
-						logger.debug("No uniprot identifiers for " + speciesName);
-					}
-				}
-				else
-				{
-					logger.debug("Species " + speciesName + " is not in the list of species known to BRENDA.");
-				}
-			}
-		}
-		else
-		{
-			logger.info("Skipping BrendaRetriever");
-		}
-	}
-	
-	/**
-	 * Execute the KEGG file retriever. It's complicated enough to warrant it's own method.
-	 */
-	private void executeKeggFileRetriever()
-	{
-		if (this.fileRetrieverFilter.contains("KEGGRetriever"))
-		{
-			logger.info("Executing KEGG retriever");
-			UniprotFileRetriever uniprotToKeggRetriever = this.uniprotFileRetrievers.get("UniProtToKEGG");
-			KEGGFileRetriever keggFileRetriever = (KEGGFileRetriever) this.fileRetrievers.get("KEGGRetriever");
-			
-			// Now we need to loop through the species.
-			String downloadDestination = keggFileRetriever.getFetchDestination();
-
-			List<Callable<Boolean>> keggJobs = new ArrayList<Callable<Boolean>>();
-			
-			for (String speciesName : objectCache.getSetOfSpeciesNames().stream().sequential()
-												.filter(speciesName -> KEGGSpeciesCache.getKEGGCodes(speciesName)!=null)
-												.collect(Collectors.toList()))
-			{
-				String speciesCode = objectCache.getSpeciesNamesToIds().get(speciesName).get(0);
-				logger.debug("Species Name: {} Species Code: {}", speciesName, speciesCode);
-				
-				Predicate<String> isValidKEGGmappingFile = new Predicate<String>()
-				{
-					@Override
-					public boolean test(String fileName)
-					{
-						try
-						{
-							return !fileName.contains(".notMapped")
-									&& fileName.contains("KEGG")
-									&& Files.exists(Paths.get(fileName))
-									&& objectCache.getSpeciesNamesToIds().get(speciesName).stream().anyMatch(s -> fileName.contains(s))
-									&& Files.lines(Paths.get(fileName)).count() > 1;
-						}
-						catch (IOException e1)
-						{
-							e1.printStackTrace();
-							return false;
-						}
-					}
-				};
-				
-				List<Path> uniProtToKeggFiles = uniprotToKeggRetriever.getActualFetchDestinations().stream()
-																			.filter(fileName -> isValidKEGGmappingFile.test(fileName))
-																			.map(fileName -> Paths.get(fileName))
-																			.collect(Collectors.toList());
-				// This could happen if the UniProt files were already downloaded. In that case, uniprotToKeggRetriever.getActualFetchDestinations() will return 
-				// NULL because nothing was downloaded this time.
-				if (uniProtToKeggFiles == null || uniProtToKeggFiles.isEmpty())
-				{
-					// Since the uniprotToKeggRetriever didn't download anything, maybe we can check in the directory and see if there are any other files there.
-					String uniProtToKeggDestination = uniprotToKeggRetriever.getFetchDestination();
-					
-					try
-					{
-						// We'll try to search for everything in the uniprotToKeggRetriever's destination's directory.
-						uniProtToKeggFiles = Files.list(Paths.get(uniProtToKeggDestination).getParent())
-													.filter(path -> isValidKEGGmappingFile.test(path.toString()) )
-													.collect(Collectors.toList());
-					}
-					catch (IOException e)
-					{
-						e.printStackTrace();
-					}
-					
-				}
-
-				if (uniProtToKeggFiles != null && uniProtToKeggFiles.size() > 0)
-				{
-					List<Path> files = uniProtToKeggFiles;
-					Callable<Boolean> job = new Callable<Boolean>()
-					{
-
-						@Override
-						public Boolean call() throws Exception
-						{
-							KEGGFileRetriever retriever = new KEGGFileRetriever(keggFileRetriever.getRetrieverName());
-							retriever.setAdapter(keggFileRetriever.getAdapter());
-							retriever.setDataURL(keggFileRetriever.getDataURL());
-							retriever.setUniprotToKEGGFiles(files);
-							retriever.setMaxAge(keggFileRetriever.getMaxAge());
-							// the ".2" is for the ReferenceDatabase - in this case it is UniProt whose DB_ID is 2.
-							retriever.setFetchDestination(downloadDestination.replaceAll(".txt", "." + speciesCode + ".2.txt"));
-							try
-							{
-								retriever.fetchData();
-							}
-							catch (Exception e)
-							{
-								e.printStackTrace();
-								throw new Error(e);
-							}
-							return true;
-						}
-					};
-					keggJobs.add(job);
-
-				}
-				else
-				{
-					logger.info("Sorry, No uniprot-to-kegg mappings found for species {} / {}", speciesName, speciesCode);
-				}
-			}
-			// These jobs are not very CPU intense so it is probably not too serious to them ALL in parallel.
-			ForkJoinPool pool = new ForkJoinPool(keggJobs.size());
-			pool.invokeAll(keggJobs);
-		}
-		else
-		{
-			logger.info("Skipping KEGGRetriever");
-		}
-	}
 
 	/**
 	 * Create references.
@@ -803,48 +550,6 @@ public class AddLinks
 				Map<String, Map<String, List<String>>> mappings = (Map<String, Map<String, List<String>>>) dbMappings.get(k);
 				((ENSMappedIdentifiersReferenceCreator)refCreator).createIdentifiers(personID, mappings, sourceReferences);
 			}
-		}
-	}
-
-	/**
-	 * Process ENSEMBL files. ENSEMBL files need special processing - you can't do it in a single step.
-	 * This function will actually run EnsemblFileAggregators and EnsemblFileAggregatorProcessors.
-	 * These two classes are used to produce an aggregate file containing ALL Ensembl mappings: each line will have the following identifiers:
-	 *  - ENSP, ENST, ENSG.
-	 *  Each line also contains the Name of an external database that the ENSG maps to, and the identifier value from that external database (or "null" if there was no mapping).
-	 * @param dbMappings - this mapping will be updated by this function.
-	 * @throws Exception
-	 * @throws InvalidAttributeException
-	 */
-	private void processENSEMBLFiles(Map<String, Map<String, ?>> dbMappings) throws Exception, InvalidAttributeException
-	{
-		@SuppressWarnings("unchecked")
-		Collection<GKInstance> enspDatabases = dbAdapter.fetchInstanceByAttribute(ReactomeJavaConstants.ReferenceDatabase, ReactomeJavaConstants.name, " LIKE ", "ENSEMBL%PROTEIN");
-		Set<String> species = new HashSet<String>();
-		for (GKInstance inst : enspDatabases)
-		{	
-			List<GKInstance> refGeneProds = objectCache.getByRefDb(inst.getDBID().toString(), "ReferenceGeneProduct");
-			for (GKInstance refGeneProd : refGeneProds)
-			{
-				species.add(((GKInstance)refGeneProd.getAttributeValue(ReactomeJavaConstants.species)).getDBID().toString());
-			}
-		}
-		
-		for (String speciesID : species/*objectCache.getSpeciesNamesByID().keySet()*/)
-		{
-			List<String> dbNames = new ArrayList<String>(Arrays.asList("EntrezGene", "Wormbase")/*objectCache.getRefDbNamesToIds().keySet()*/);
-			EnsemblFileAggregator ensemblAggregator = new EnsemblFileAggregator(speciesID, dbNames, "/tmp/addlinks-downloaded-files/ensembl/");
-			ensemblAggregator.createAggregateFile();
-			
-			EnsemblAggregateFileProcessor aggregateProcessor = new EnsemblAggregateFileProcessor("file-processors/EnsemblAggregateFileProcessor");
-			aggregateProcessor.setPath(Paths.get("/tmp/addlinks-downloaded-files/ensembl/"+ "ensembl_p2xref_mapping."+speciesID+".csv") );
-			aggregateProcessor.setMode(EnsemblAggregateProcessingMode.XREF);
-			Map<String, Map<String, List<String>>> xrefMapping = aggregateProcessor.getIdMappingsFromFile();
-			dbMappings.put("ENSEMBL_XREF_"+speciesID, xrefMapping);
-			
-			aggregateProcessor.setMode(EnsemblAggregateProcessingMode.ENSP_TO_ENSG);
-			Map<String, Map<String, List<String>>> ensp2EnsgMapping = aggregateProcessor.getIdMappingsFromFile();
-			dbMappings.put("ENSEMBL_ENSP_2_ENSG_"+speciesID, ensp2EnsgMapping);
 		}
 	}
 
@@ -1054,55 +759,6 @@ public class AddLinks
 			}
 		);
 		return dbMappings;
-	}
-
-	/**
-	 * Execute file retrievers. Covers pretty much everything, except for ENSEMBL and UniProt retrievers. 
-	 */
-	private void executeSimpleFileRetrievers()
-	{
-		fileRetrievers.keySet().stream().parallel()
-										.filter(k -> !k.equals("KEGGRetriever") && !k.equals("BrendaRetriever"))
-										.forEach(k ->
-		{
-			// KEGGRetreiver is special: it depends on the result of the uniprotToKegg retriever as an input, so we can't execute it here.
-			if (fileRetrieverFilter.contains(k))
-			{
-				FileRetriever retriever = fileRetrievers.get(k);
-				logger.info("Executing downloader: {}",k);
-				try
-				{
-					retriever.fetchData();
-					logger.info("Completed downloader: {}",k);
-				}
-				catch (Exception e)
-				{
-					
-					//TODO: The decision to continue after a failure should be a configurable option. 
-					logger.warn("Exception caught while processing {}, message is: \"{}\". Will continue with next file retriever.",k,e.getMessage());
-					e.printStackTrace();
-				}
-			}
-			else
-			{
-				logger.info("Skipping {}",k);
-			}
-		});
-	}
-
-	/**
-	 * Execute the UniProt retrievers.
-	 * @param numberOfUniprotDownloadThreads
-	 */
-	private void executeUniprotFileRetrievers(int numberOfUniprotDownloadThreads)
-	{
-		logger.info("Executing UniProt file retrievers");
-		UniProtFileRetrieverExecutor executor = new UniProtFileRetrieverExecutor();
-		executor.setFileRetrieverFilter(fileRetrieverFilter);
-		executor.setObjectCache(objectCache);
-		executor.setUniprotFileRetrievers(uniprotFileRetrievers);
-		executor.setNumberOfUniprotDownloadThreads(numberOfUniprotDownloadThreads);
-		executor.execute();
 	}
 
 	public void setObjectCache(ReferenceObjectCache objectCache)
