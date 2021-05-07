21edd94363b19aa93db60381d927e3ed6c0e68f1
SolomonShorser-OICR <solomon.shorser@oicr.on.ca>
2021-05-07 16:49:32 -0400

    Start adding GtP functionality.

---
+++ src/main/java/org/reactome/addlinks/fileprocessors/gtp/GuideToPharmacologyTargetsFileProcessor.java
@@ -0,0 +1,15 @@
+package org.reactome.addlinks.fileprocessors.gtp;
+
+import java.util.Map;
+
+import org.reactome.addlinks.fileprocessors.FileProcessor;
+
+public class GuideToPharmacologyTargetsFileProcessor extends FileProcessor {
+
+	@Override
+	public Map getIdMappingsFromFile() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
