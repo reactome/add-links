e204df5a45f94d51e09920bda012df11e0f6a835
Joel Weiser <jweiser13@protonmail.com>
2023-07-11 10:55:51 -0400

    bug fix - updates GtPLigandsFileProcessor to use proper bean and ChEBI id without prefix

--- src/main/java/org/reactome/addlinks/fileprocessors/gtp/GuideToPharmacologyLigandsFileProcessor.java
+++ src/main/java/org/reactome/addlinks/fileprocessors/gtp/GuideToPharmacologyLigandsFileProcessor.java
@@ -11,25 +11,25 @@ import static org.reactome.addlinks.fileprocessors.gtp.Utils.getCSVParser;
 
 public class GuideToPharmacologyLigandsFileProcessor extends FileProcessor<String> {
 
-	public GuideToPharmacologyLigandsFileProcessor()
-	{
+	public GuideToPharmacologyLigandsFileProcessor() {
 		super(null);
 	}
 
-	public GuideToPharmacologyLigandsFileProcessor(String processorName)
-	{
+	public GuideToPharmacologyLigandsFileProcessor(String processorName) {
 		super(processorName);
 	}
 
 	@Override
 	public Map<String, String> getIdMappingsFromFile() {
 		Map<String, String> mapping = new HashMap<>();
+
 		try(CSVParser parser = getCSVParser(this.pathToFile)) {
 			parser.forEach(line -> {
 				// We need the ligand ID
 				String ligandID = line.get("Ligand id");
-				// we will try to map with ChEBI ID
-				String chebiID = line.get("Chebi ID");
+				// we will try to map with ChEBI ID removing the "CHEBI:" prefix
+				String chebiID = line.get("Chebi ID").replace("CHEBI:","");
+
 				// but for future reference, some entities don't have ChEBI IDs but they may have CHEMBL, PubChem, or UniProt
 				// identifiers so you could also extract those mappings. It will make reference creation more complicated,
 				// and it may be better to have a different file-process/reference-creator pair for the different sources that map to GtP
