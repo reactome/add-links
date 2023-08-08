2edbc2d9f778c7463a81fed06627465b4ad5f603
Joel Weiser <jweiser13@protonmail.com>
2023-08-08 10:30:11 -0400

    split method to process mapping files

--- src/main/java/org/reactome/addlinks/fileprocessors/PharmacoDBFileProcessor.java
+++ src/main/java/org/reactome/addlinks/fileprocessors/PharmacoDBFileProcessor.java
@@ -35,27 +35,6 @@ public class PharmacoDBFileProcessor extends FileProcessor<String> {
 		this.guideToPharmacologyFilePath = path;
 	}
 	
-	/**
-	 * Processes a CSV file, assumed to have the first row as the Header. A map will be returned, keyed by
-	 * <code>mappingSourceField</code> with the values coming from <code>mappingTargetField</code>.
-	 * @param path - the path to the file to process.
-	 * @param mappingSourceField - the NAME of the source mapping field.
-	 * @param mappingTargetField - the NAME of the target mapping field.
-	 * @return A map, which maps <code>mappingSourceField</code> to <code>mappingTargetField</code>.
-	 */
-	private Map<String, String> processFile(Path path, String mappingSourceField, String mappingTargetField) {
-		Map<String, String> mapping = new HashMap<>();
-		
-		try(CSVParser parser =
-				new CSVParser(new FileReader(path.toString()), CSVFormat.DEFAULT.withFirstRecordAsHeader())) {
-			parser.forEach(record -> mapping.put(record.get(mappingSourceField), record.get(mappingTargetField)));
-		} catch (IOException e) {
-			logger.error("IOException was caught: ", e);
-		}
-		
-		return mapping;
-	}
-	
 	@Override
 	public Map<String, String> getIdMappingsFromFile() {
 		// First, we need to use Guide To Pharmacology file to create Guide To Pharmacology->PubChem mapping.
@@ -63,13 +42,13 @@ public class PharmacoDBFileProcessor extends FileProcessor<String> {
 		// The result should be Guide To Pharmacology->PharmacoDB mapping.
 		
 		// Process Guide To Pharmacology file
-		Map<String, String> guideToPharmacology2PubChem =
-			this.processFile(this.guideToPharmacologyFilePath, "Ligand id", "PubChem CID");
+		Map<String, String> guideToPharmacology2PubChem = processGuideToPharmacologyFile();
+//			this.processFile(this.guideToPharmacologyFilePath, "Ligand id", "PubChem CID");
 		logger.info("There are {} Guide To Pharmacology->PubChem mappings", guideToPharmacology2PubChem.keySet().size());
 		
 		// Process PharmacoDB file.
-		Map<String, String> pubChem2Pharmacodb =
-			this.processFile(this.pharmacodbFilePath, "cid", "PharmacoDB.uid");
+		Map<String, String> pubChem2Pharmacodb = processPharmacoDBFile();
+//			this.processFile(this.pharmacodbFilePath, "cid", "PharmacoDB.uid");
 		logger.info("There are {} PubChem->PharmacoDB mappings", pubChem2Pharmacodb.keySet().size());
 		
 		Map<String, String> guideToPharmacology2PharmacoDB = new HashMap<>(guideToPharmacology2PubChem.keySet().size());
@@ -94,4 +73,53 @@ public class PharmacoDBFileProcessor extends FileProcessor<String> {
 		logger.info("There are {} Guide To Pharmacology->PharmacoDB mappings.", guideToPharmacology2PharmacoDB.size());
 		return guideToPharmacology2PharmacoDB;
 	}
+
+//	/**
+//	 * Processes a CSV file, assumed to have the first row as the Header. A map will be returned, keyed by
+//	 * <code>mappingSourceField</code> with the values coming from <code>mappingTargetField</code>.
+//	 * @param path - the path to the file to process.
+//	 * @param mappingSourceField - the NAME of the source mapping field.
+//	 * @param mappingTargetField - the NAME of the target mapping field.
+//	 * @return A map, which maps <code>mappingSourceField</code> to <code>mappingTargetField</code>.
+//	 */
+//	private Map<String, String> processFile(Path path, String mappingSourceField, String mappingTargetField) {
+//		Map<String, String> mapping = new HashMap<>();
+//
+//		try(CSVParser parser =
+//				new CSVParser(new FileReader(path.toString()), CSVFormat.DEFAULT.withFirstRecordAsHeader())) {
+//			parser.forEach(record -> mapping.put(record.get(mappingSourceField), record.get(mappingTargetField)));
+//		} catch (IOException e) {
+//			logger.error("IOException was caught: ", e);
+//		}
+//
+//		return mapping;
+//	}
+
+	private Map<String, String> processPharmacoDBFile() {
+        Map<String, String> mapping = new HashMap<>();
+
+        try(CSVParser parser = new CSVParser(
+            new FileReader(this.pharmacodbFilePath.toString()), CSVFormat.DEFAULT.withFirstRecordAsHeader())
+        ) {
+            parser.forEach(record -> mapping.put(record.get("cid"), record.get("PharmacoDB.uid")));
+        } catch (IOException e) {
+            logger.error("IOException was caught: ", e);
+        }
+
+        return mapping;
+    }
+
+    private Map<String, String> processGuideToPharmacologyFile() {
+        Map<String, String> mapping = new HashMap<>();
+
+        try(CSVParser parser = new CSVParser(
+            new FileReader(this.guideToPharmacologyFilePath.toString()), CSVFormat.DEFAULT.withFirstRecordAsHeader())
+        ) {
+            parser.forEach(record -> mapping.put(record.get("Ligand id"), record.get("PubChem CID")));
+        } catch (IOException e) {
+            logger.error("IOException was caught: ", e);
+        }
+
+        return mapping;
+    }
 }
